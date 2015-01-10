996668fe84eba0195bd59c5c82718ad052dd3517
jadler05 <james.adler@tufts.edu>
2015-01-09 22:34:01 -0500

    Adler

---
+++ src/utilities/aux.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+
+/* ===================================================== */
+#include "vec.h"
+/*=============================================================*/
+
+/*** Auxillary Files (from Ludmil) *******************************************************/
+
+/****************************************************************************************/
+void rveci_(FILE *fp, INT *vec, INT *nn)       
+/* reads a vector of integers of size nn from a file fp*/
+{
+	
+  INT n;
+  INT *vec_end;
+  n = *nn;
+  vec_end  =  vec + n;
+  for ( ; vec < vec_end; ++vec)
+    fscanf(fp,"%i",vec);
+  //fprintf(stdout,"Read %d INTEGERS", n);
+  return;
+}
+/****************************************************************************************/
+
+/****************************************************************************************/
+void rvecd_(FILE *fp,  REAL *vec, INT *nn)
+/* reads a vector of REALS of size nn from a file fp*/
+{
+  INT n;
+  REAL *vec_end;  
+  n= *nn;
+  vec_end =  vec + n;
+  for ( ; vec < vec_end; ++vec)
+    fscanf(fp,"%lg",vec);
+  //fprintf(stdout,"Read %d REALS", n);
+  return;
+}
+/****************************************************************************************/
