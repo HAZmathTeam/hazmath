af5f277423815bb031de568935d139d308ee366c
LUDMIL ZIKATANOV <ltz1@psu.edu>
2017-03-08 01:12:35 -0500

    multigraph compiles but does not converge for some crazy matrices

--- examples/Solve_Multigraph/multigraph_solve.c
+++ examples/Solve_Multigraph/multigraph_solve.c
@@ -6,61 +6,63 @@
 #include <limits.h>
 
 #include "hazmat.h"
-
-void  mginit_(INT *n, INT *ispd, INT *nblock, INT *iblock,		\
-	      INT *maxja, INT *jareb, INT *maxa, REAL *areb,		\
-	      INT *ncfact, INT *maxlvl,	INT *maxfil, INT *ka,
-	      INT *lvl, REAL *dtol, INT *method, 
-	      INT *iflag );
-  /*        subroutine mginit(n,ispd,nblock,ib,maxja,ja,maxa,a,ncfact,
-	    +      maxlvl,maxfil,ka,lvl,dtol,method,iflag)
-  */
-  ///  call mg(ispd,lvl,mxcg,eps1,jareb,areb,z,rhs,
-  ///	  >     ka,relerr,iflag,z1,hist)
-void mg_(INT *ispd, INT *lvl, INT *mxcg, REAL *eps1,			\
-	 INT *jareb, REAL *areb,		\
-	 REAL *sol, REAL *rhs, INT *ka, REAL *relerr,		\
-	 INT *iflag, REAL *hist );
-  /*   
-       subroutine mg(ispd,lvl,mxcg,eps1,ja,a,dr,br,ka,relerr,
-       +      iflag,hist)
-  */
+#include "multigraph_solve.h"
 
 int main (int argc, char* argv[])
 {
-  // io routines for debugging. 
-  FILE* fp;
-  fp=HAZ_fopen( char *fname, char *mode );
-  rveci_()
-#ifndef USE_SUITESPARSE
-  INT *ia=NULL, *ja;
-  double *A=NULL, *b=NULL;
+  dCSRmat A; //matrix
+  dCOOmat B; //matrix
+  dvector rhs,sol,exsol; //RHS,solution,exact solution
+  INT idummy[3],i=3,n=-16,ncol=-16;
+  // read the matrix
+  FILE* fp=HAZ_fopen("symm.coo","r");
+  rveci_(fp,idummy, &i);
+  n=A.row=B.row=idummy[0];
+  ncol=A.col=B.col=idummy[1];
+  INT m=ncol,n1=n+1;
+  A.nnz=B.nnz=idummy[2];
+  B.rowind=(INT *) calloc(B.nnz,sizeof(INT));
+  B.colind=(INT *) calloc(B.nnz,sizeof(INT));
+  B.val=(REAL *) calloc(B.nnz,sizeof(REAL));
+  
+  rveci_(fp,B.rowind, &B.nnz);
+  rveci_(fp,B.colind, &B.nnz);
+  rvecd_(fp,B.val, &B.nnz);
+  //rhs and exact sol
+  sol.row=rhs.row=exsol.row=n;
+  rhs.val=(REAL *) calloc(rhs.row,sizeof(REAL));
+  sol.val=(REAL *) calloc(sol.row,sizeof(REAL));
+  exsol.val=(REAL *) calloc(exsol.row,sizeof(REAL));
+  rvecd_(fp,rhs.val, &n);
+  rvecd_(fp,exsol.val, &n);
+  for(i=0;i<sol.row;i++) sol.val[i]=0.;
+  fclose(fp);
+  //shift
+  for(i=0;i<B.nnz;i++) B.rowind[i]--;
+  for(i=0;i<B.nnz;i++) B.colind[i]--;
+  //A in csr format
+  A.IA=(INT *) calloc((A.row+1),sizeof(INT));
+  A.JA=(INT *) calloc(A.nnz,sizeof(INT));
+  A.val=(REAL *) calloc(A.nnz,sizeof(REAL));
+  dcoo_2_dcsr (&B,&A);
+  if(B.rowind) free(B.rowind); if(B.colind) free(B.colind); if(B.val) free(B.val);
+  fprintf(stdout,"\nrows=%i,cols=%i,nnz=%i\n",A.row,A.col,A.nnz);
   // use now multigraph
-  clock_t clk_mesh_start = clock(); // Time mesh generation FE setup
+  clock_t clk_mesh_start = clock(); // Time
   INT    *jareb=NULL;
   REAL *areb=NULL;
-  INT i,nnzlu=-16,n=*nrow,n1=*nrow+1;
-  FILE *fp;
+  INT nnzlu=-16;  
   fprintf(stdout," *** Starting multigraph Solver\n");
-  csrreb(&n,&n, &nnzlu,ia,ja,a,&jareb,&areb);
-  INT maxja = 5*(n1 + jareb[n]-jareb[0]);
+  csrreb(&n,&n, &nnzlu,A.IA,A.JA,A.val,&jareb,&areb);
+  INT maxja = 4*(n1 + jareb[n]-jareb[0]);
   if(maxja < 7*n) maxja=7*n;
   INT maxa = 5*maxja;
   jareb=(INT *)realloc(jareb,maxja*sizeof(INT));
-  areb=(REAL *)realloc(areb,maxa*sizeof(INT));
+  areb=(REAL *)realloc(areb,maxa*sizeof(REAL));
   INT  nblock = 1;
   INT iblk[2]={0,0};
   iblk[nblock]=n1;
   iblk[nblock-1]=1;
-  INT ispd=0;
-  INT ncfact=4;
-  INT maxlvl=20;
-  INT maxfil=32;
-  INT method=0;
-  REAL dtol=1e-2;
-  INT iflag=-16;  
-  /// outputs
-  INT lvl=-16;
   INT *ka = (INT *) calloc(10*(maxlvl+1),sizeof(INT));
   //shift and do mginit
   for (i=0;i<n1+nnzlu;++i)
@@ -70,52 +72,32 @@ int main (int argc, char* argv[])
   	  &ncfact, &maxlvl, &maxfil, ka,
   	  &lvl, &dtol, &method, 
   	  &iflag );
+  fprintf(stdout,"\n\n*** mginit: flag=%i\n\n",iflag);
+  fflush(stdout);
   INT j, ij;
-  REAL eps1=1e-8;
-  INT mxcg=1000;
   INT ns=ka[10*(lvl+1-1)+(2-1)]-1;
-  REAL *hist= (REAL *) calloc(maxlvl+3,sizeof(REAL));
-  REAL relerr=1e0;  
+  REAL *hist= (REAL *) calloc(22,sizeof(REAL));
   mg_(&ispd, &lvl, &mxcg, &eps1, jareb, areb,	\
-       sol, rhs, ka, &relerr, &iflag, hist );
+       sol.val, rhs.val, ka, &relerr, &iflag, hist );
+  fprintf(stdout,"\n\n*** mg_: flag=%i\n\n",iflag);
+  fflush(stdout);
   /* unshift */
   for (i=0;i<n1+nnzlu;++i) jareb[i]-=1;
-  if(exsol){
-    REAL err0=fabs(sol[0]-exsol[0]);
-    for(j=1; j<n;++j){
-      if(err0<fabs(sol[j]-exsol[j])){
-	err0=fabs(sol[j]-exsol[j]);
-      }
-    }
-    fprintf(stdout,"\n Rel. Err.=%12.5e, err_infty=%12.5e\n\n",relerr, err0);
-  }else{
-    fprintf(stdout,"\n Rel. Err.=%12.5e\n\n",relerr);
-  }
-  //ka is the array which contains all info. 
-  INT iend=20, itnum= (INT )hist[iend];
+  INT iend=hist[22], itnum= (INT )hist[iend];
   if(itnum<iend) iend=itnum;
   fprintf(stdout,"\nMultigraph history (iter_num=%5i)\n",itnum);    
   for (i=0;i<iend;++i){    
     fprintf(stdout,"iter =  %5i; res %12.4e\n",itnum-iend+i+1,hist[i]);    
   }
-  //    fprintf(stdout,"iflag=%i ; lvl= %i\n",iflag,lvl);
-  //  for (i=0;i<n1+nnzlu;++i)jareb[i]-=1;
-  ///  fprintf(stdout,"\n");
+  //  for (i=0;i<n;++i){    
+  //    fprintf(stdout,"%22.16e\n",sol.val[i]);    
+  //  }
   if(ka) free(ka);
-  if(khist) free(ka);
-  //  if(z) free(z);
-  if(jareb) free(jareb); /* these are all integers */
-  if(areb) free(areb); /*this should be all  reals */
-#else
-   // use UMFPACK to solve the system.
-   UMFPackSolver umf_solver;
-   umf_solver.SetOperator(A);
-   umf_solver.Mult(*b, x);
-#endif
-  return;
+  if(hist) free(hist);
+  if(jareb) free(jareb);
+  if(areb) free(areb); 
+  return 0;
 }
-
-
 #if defined (__cplusplus)
 }
 #endif
