a0fd66b9282a39658dd7564ad313d0b09cd7f2cd
LUDMIL ZIKATANOV <ltz1@psu.edu>
2017-03-09 18:25:51 -0500

    compiled conv_diff for first time
    Former-commit-id: c5816a4a5a2a173b81ca984048b802f56c8b2c61 [formerly 758b416c49399ca22250070f1a99d83617b69142]
    Former-commit-id: ac312d154d3454bfd3d1fddecc4e602ddfd9e92e

--- examples/ConvectionDiffusion/ConvectionDiffusion.h
+++ examples/ConvectionDiffusion/ConvectionDiffusion.h
@@ -2,34 +2,36 @@
 #undef WITH_SUITESPARSE
 #endif
 
-REAL eps0=1e-6;
-REAL pi=3.141592653589793e0;
+#ifndef M_PI
+#define M_PI 3.141592653589793e+00;
+#endif
 
 // PDE Coefficients
-void diffusion_coeff(REAL *val,REAL* x) {
+void diffusion_coeff(REAL *val,REAL* x, REAL t) {
   // a(x)
   *val = 1.0;
+  return;
 }
 
 // Exact Solution (if you have one)
 // Change as needed for different dimensions
-void exactsol(REAL *val,REAL* x0) {
+void exactsol(REAL *val,REAL* x, REAL t) {
   // 1D
   //*val = sin(M_PI*x[0])*exp(-M_PI*M_PI*time);
   // 2D
-  *val = sin(M_PI*x[0])*sin(M_PI*x[1])*exp(-2.0*M_PI*M_PI*time);
+  *val = sin(M_PI*x[0])*sin(M_PI*x[1]);
   // 3D
-  ///  *val=x0[2]*x0[2];
-  //  *val=10.*x0[0]*(1.-x0[0]) *x0[1]*(1.-x0[1])*x0[2];
-  // *val= x0[2]+x0[0];
-  //  *val=exp(-x0[2])*sin(pi*x0[0])*sin(pi*x0[1]);
-  //*val = sin(M_PI*x[0])*sin(M_PI*x[1])*sin(M_PI*x[2])*exp(-3*M_PI*M_PI*time);
+  ///  *val=x[2]*x[2];
+  //  *val=10.*x[0]*(1.-x[0]) *x[1]*(1.-x[1])*x[2];
+  // *val= x[2]+x[0];
+  return;
 }
 
-void advection(const REAL* x0, REAL *advcoeff) {
-  advcoeff[0] = 1e2; // or beta_1(x0)
-  advcoeff[1] = 0.; // or beta_2(x0)
-  advcoeff[2] = -1.; // or beta_3(x0)
+void advection(REAL* x, REAL *advcoeff, REAL t) {
+  advcoeff[0] = 1e2; // or beta_1(x)
+  advcoeff[1] = 0.; // or beta_2(x)
+  advcoeff[2] = -1.; // or beta_3(x)
+  return;
 }
 
 REAL bernoulli(const REAL z)
@@ -44,33 +46,15 @@ REAL bernoulli(const REAL z)
     return 0.;
 }
 // Right-hand Side
-void myrhs(REAL *val,REAL* x) {
+void myrhs(REAL *val,REAL* x, REAL t) {
   *val = 0.0;
 }
 
 // Boundary Conditions
-void bc(REAL *val, REAL* x) {
+void bc(REAL *val, REAL* x, REAL t) {
   *val= 0.0;
+  return;
 }
 
-REAL f_rhs(REAL* x0){
-  //  return -2.*eps0+2*x0[2]; // -\Delta u-eps0*u_{tt}
-  //  return 2.*x0[2]; // pretend eps0 is 0 and u_{tt} is small. 
-  //  return 20.*x0[1]*(1.-x0[1]) + 20.*x0[0]*(1.-x0[0])*x0[2];
-  //   return (2.*pi*pi-1.)*exp(-x0[2])*sin(pi*x0[0])*sin(pi*x0[1]);
-    return 1e+00;
-}
-////////////////////////////////////////////////////////////
-void DiffusionTensor(const REAL* x0, REAL *alpha) {
-  /* 
-     alpha is a full matrix, in C stored by rows:
-     alpha[i][j] <-- alpha[i*n+j]
-   */
-  INT i,n=x0.row;
-  for (i=0;i<n*n;i++) alpha[i]=0.;
-  alpha[0*n+0]=1.; //(1,1)
-  alpha[1*n+1]=1.; //(2,2)
-  alpha[2*n+2]=1.; //(3,3)
-}
 
 
