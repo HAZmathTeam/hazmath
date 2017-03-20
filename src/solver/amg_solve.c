c7844d2279017468dd976b35d7f73fe1c3c2445a
LUDMIL ZIKATANOV <ltz1@psu.edu>
2017-03-20 17:44:07 -0400

    removed inl files
    Former-commit-id: 56818028a3158f4108bf6fac6c1bd950f06f5293 [formerly b58743505827d44803c51d4396d38b15027947c5]
    Former-commit-id: fa7424d6ab7506ccb1467176fca20f6f3e83c71d

--- src/solver/amg_solve.c
+++ src/solver/amg_solve.c
@@ -10,7 +10,91 @@
  */
 
 #include "hazmath.h"
-#include "itsolver_util.inl"
+/*! \file itsolver_util.inl
+ *
+ *  Created by James Adler, Xiaozhe Hu, and Ludmil Zikatanov on 5/13/15.
+ *  Copyright 2015__HAZMATH__. All rights reserved.
+ *
+ *  \note  Done cleanup for releasing -- Xiaozhe Hu 03/12/2017
+ *
+ */
+
+/*---------------------------------*/
+/*--      Private Functions      --*/
+/*---------------------------------*/
+
+//! Warning for residual false convergence
+#define ITS_FACONV  printf("### HAZMATH WARNING: False convergence!\n")
+
+//! Warning for solution close to zero
+#define ITS_ZEROSOL printf("### HAZMATH WARNING: Iteration stopped due to the solution is almost zero! %s : %d\n", __FUNCTION__, __LINE__)
+
+//! Warning for iteration restarted
+#define ITS_RESTART printf("### HAZMATH WARNING: Iteration restarted due to stagnation! %s : %d\n", __FUNCTION__, __LINE__)
+
+//! Warning for stagged iteration
+#define ITS_STAGGED printf("### HAZMATH WARNING: Iteration stopped due to staggnation! %s : %d\n", __FUNCTION__, __LINE__)
+
+//! Warning for tolerance practically close to zero
+#define ITS_ZEROTOL printf("### HAZMATH WARNING: The tolerence might be too small! %s : %d\n", __FUNCTION__, __LINE__)
+
+//! Warning for divided by zero
+#define ITS_DIVZERO printf("### HAZMATH WARNING: Divided by zero! %s : %d\n", __FUNCTION__, __LINE__)
+
+//! Warning for actual relative residual
+#define ITS_REALRES(relres) printf("### HAZMATH WARNING: The actual relative residual = %e!\n",(relres))
+
+//! Warning for computed relative residual
+#define ITS_COMPRES(relres) printf("### HAZMATH WARNING: The computed relative residual = %e!\n",(relres))
+
+//! Warning for too small sp 
+#define ITS_SMALLSP printf("### HAZMATH WARNING: sp is too small! %s : %d\n", __FUNCTION__, __LINE__)
+
+//! Warning for restore previous iteration 
+#define ITS_RESTORE(iter) printf("### HAZMATH WARNING: Restore iteration %d!\n",(iter));
+
+//! Output relative difference and residual
+#define ITS_DIFFRES(reldiff,relres) printf("||u-u'|| = %e and the comp. rel. res. = %e.\n",(reldiff),(relres));
+
+//! Output L2 norm of some variable
+#define ITS_PUTNORM(name,value) printf("L2 norm of %s = %e.\n",(name),(value));
+
+/**
+ * \fn inline static void ITS_CHECK (const INT MaxIt, const REAL tol)
+ * \brief Safeguard checks to prevent unexpected error for iterative solvers
+ *
+ * \param MaxIt   Maximal number of iterations
+ * \param tol     Tolerance for convergence check
+ *
+ */
+inline static void ITS_CHECK (const INT MaxIt, const REAL tol)
+{    
+    if ( tol < SMALLREAL ) {
+        printf("### HAZMATH WARNING: Convergence tolerance for iterative solver is too small!\n");
+    }
+    if ( MaxIt <= 0 ) {
+        printf("### HAZMATH WARNING: Max number of iterations should be a POSITIVE integer!\n");
+    }
+}
+
+/**
+ * \fn inline static void ITS_FINAL (const INT iter, const INT MaxIt, const REAL relres) 
+ * \brief Print out final status of an iterative method
+ *
+ * \param iter    Number of iterations
+ * \param MaxIt   Maximal number of iterations
+ * \param relres  Relative residual 
+ *
+ */
+inline static void ITS_FINAL (const INT iter, const INT MaxIt, const REAL relres) 
+{
+    if ( iter > MaxIt ) {
+        printf("### HAZMATH WARNING: Max iter %d reached with rel. resid. %e.\n", MaxIt, relres);
+    }
+    else if ( iter >= 0 ) {
+        printf("Number of iterations = %d with relative residual %e.\n", iter, relres);
+    }
+}
 
 /*---------------------------------*/
 /*--      Public Functions       --*/
