23138f140cb768d742d6fc99613afd7a270b8281
LUDMIL ZIKATANOV <ltz1@psu.edu>
2017-03-27 06:51:30 -0600

    added error ckecks to tarmk.sh
    Former-commit-id: 79612b48a273715b711e348ed6d41b8c5e1ca1e5 [formerly d33fd6a809eff8946b3d966ac111c1b106312734]
    Former-commit-id: c6e8d17ffa536b375dc2e6b59e4f119f03a62c6b

--- haz_shutils/tarmk.sh
+++ haz_shutils/tarmk.sh
@@ -3,22 +3,31 @@
 # usage is tarmk.sh 
 # Edit to include various directories and files into tarball that will be exhanged to users
 
+
 VERSION="0.0.0"
 DNAME="hazmath-${VERSION}"
 ##FILENAME="hazmath_$(date +%Y_%m_%d)_$VERSION.tar.gz"
 ###FILENAME="${DNAME}_$(date +%Y_%m_%d).tar.gz"
 ## for not changing the repo every time, we have a constant name
 ## independent of the date.
+
 FILENAME="${DNAME}.tar.gz"
 
-echo "Creating Files for $DNAME on $(date +%Y_%m_%d)"
+[ -f ${FILENAME}  ] && /bin/rm -rf ${FILENAME}
 
-set -x
+echo "Creating TAR file with the current version of HAZMATH: $DNAME on $(date +%Y_%m_%d)"
+
+#set -x
 
 tar \
     -s:^\.\.\/:${DNAME}/: --exclude-from="tar.excl" \
     -zcvf ${FILENAME} ../
 
-set +x
+#set +x
 
-echo "$FILENAME created."
+if [ $? -eq  0 ] ;  then
+       echo "*** $FILENAME successfully created. ***"
+else
+      echo "*** ERROR: ***  the shell exited with a nonzero flag during creation of the archive: ${FILENAME}"
+      echo "NO tar file created"
+fi
