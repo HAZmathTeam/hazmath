459a2976af9793c17336b547aacb63620a1609d2
LUDMIL ZIKATANOV <ltz1@psu.edu>
2017-04-14 12:59:27 -0400

    updated tarmk.sh to add the "examples/*/output" directories
    Former-commit-id: 6403f4f55cf7fa1d81c71104853b0097042ec572 [formerly d687f720314e4d9ba62772540b91b7b1231d4c11]
    Former-commit-id: 81c1d5afc69e6a0572064d2c3f551422b9154102

--- haz_shutils/tarmk.sh
+++ haz_shutils/tarmk.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # 
-# usage is tarmk.sh 
-# Edit to include various directories and files into tarball that will be exhanged to users
+# usage: ./tarmk.sh 
+# Creates a tar file with the latest hazmath sources and examples. 
 
 
 VERSION="0.0.0"
@@ -11,17 +11,31 @@ DNAME="hazmath-${VERSION}"
 ## for not changing the repo every time, we have a constant name
 ## independent of the date.
 
-FILENAME="${DNAME}.tar.gz"
+FILENAME="${DNAME}.tar"
 
-[ -f ${FILENAME}  ] && /bin/rm -rf ${FILENAME}
-
-echo "Creating TAR file with the current version of HAZMATH: $DNAME on $(date +%Y_%m_%d)"
+FILENAMEz="${FILENAME}.gz"
 
 #set -x
 
+[ -f ${FILENAME} ] && /bin/rm -rf  ${FILENAME}
+
+[ -f ${FILENAMEz} ] && /bin/rm -rf ${FILENAMEz}
+
+echo "Creating TAR file with the current version of HAZMATH: $DNAME on $(date +%Y_%m_%d)"
+
 tar \
-    -s:^\.\.\/:${DNAME}/: --exclude-from="tar.excl" \
-    -zcvf ${FILENAME} ../
+    -s:^\.\.:${DNAME}: --exclude-from="tar.excl" \
+    -cvf ${FILENAME} ../
+
+echo "Adding directories needed for output to ${FILENAME}"
+
+tar --no-recursion \
+    -s:^\.\.:${DNAME}: --exclude-from="tar.excl" \
+    -rvf ${FILENAME} ../examples/*/output
+
+echo "Compressing: ${FILENAME} --> ${FILENAMEz}"
+
+[ -f  ${FILENAME} ]  && gzip -S'.gz' -f ${FILENAME}
 
 #set +x
 
