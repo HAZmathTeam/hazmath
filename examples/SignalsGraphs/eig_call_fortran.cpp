8cf3c894388a7ee707238b282f7159b0870be490
Ludmil Zikatanov <ludmil@zsolvers.net>
2017-11-10 20:55:58 -0500

    simplified makefile a bit more and removed the un-necessary includes from eigenvalue driver
    Former-commit-id: cc6b7609250efee3a948e09b10db3cea25aa8da1 [formerly f2a69be7ab56e5adec589cc6a649c6c5b3395150]
    Former-commit-id: c91ee71ad9c5dd081f5172df7560f7dc54270565

--- examples/SignalsGraphs/eig_call_fortran.cpp
+++ examples/SignalsGraphs/eig_call_fortran.cpp
@@ -2,20 +2,11 @@
 extern "C" {
 #endif
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <float.h>
-#include <limits.h>
 
   void  dsyev_(char *chv, char *chu, int *ni, double *a, int *nj, double *w, double *work, int *n5, int *info );
   
   void drvdsyev(int ni, double *a, int nj, double *w, int *info ){
-    // char *chv=(char *)malloc(sizeof(char));
-    // char *chu=(char *)malloc(sizeof(char));
-    // chv="V";
-    // chu="U";
     int n5=5*ni;
     char chu='U',chv='V';
     double *work=(double *)calloc(n5,sizeof(double));
