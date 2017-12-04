ec060600a6c30f84676af7ccbef0559bb7dda749
Ludmil Zikatanov <ludmil@zsolvers.net>
2017-12-04 12:33:32 -0500

    changes related to hydrolena; scomplex now has a member fvals to hold the elevations of every point; or any function value
    Former-commit-id: e0647f80800c0886e4a5a368ef85eae30174630c [formerly 704b41e6b71329bb559dd806a444da79d8088723]
    Former-commit-id: 03199c77e41e2ae13f4025cdc61898e3b2092f8a

--- haz_shutils/headmk.sh
+++ haz_shutils/headmk.sh
@@ -5,10 +5,11 @@
 # the function names from src/*/*.c are put into the include/hazmath.h
 # FORTRAN: the subroutine names src/amr/*.f are put into the include/fortran_headers.h
 set +x
-/bin/cat $1/src/assemble/*.c $1/src/fem/*.c \
+
+cat $1/src/assemble/*.c $1/src/fem/*.c \
     $1/src/grid/*.c $1/src/nonlinear/*.c $1/src/solver/*.c  $1/src/utilities/*.c $1/src/timestepping/*.c $1/src/interfaces/*.c  $1/src/amr/*.c \
 	| awk -v name="hazmath.h" -f mkheaders.awk > $1/include/hazmath.h
 
-/bin/sed -f createh.sed $1/src/amr/*.f	> $1/include/fortran_headers.h
+sed -f createh.sed $1/src/amr/*.f	> $1/include/fortran_headers.h
 
 set -x
