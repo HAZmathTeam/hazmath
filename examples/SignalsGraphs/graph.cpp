57d4caea74e424c12fdc43c034862333207c0f71
Wenfang Xu <xu.wnfng@gmail.com>
2017-12-13 19:54:32 -0500

    rewrote code to get rid of boost dependency
    Former-commit-id: 540b2ea2b3b17a77743ae35a68f8d117cba7c16a [formerly 84caa252654fc8a5ed345d28ce9d0a2ad3ef41e1]
    Former-commit-id: 636495ca2cc96dde779f29f8a7d4433f430bc50a

--- examples/SignalsGraphs/graph.cpp
+++ examples/SignalsGraphs/graph.cpp
@@ -1,40 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <algorithm>
 #include <unordered_map>
+#include <set>
 #include <cassert>
 
 #include "graph.hpp"
-#include "boost/unordered_map.hpp"
 
 using namespace std;
-
-void Aggregate::Merge(const AggregatePtr& agg) {
-  this->vertices.insert(agg->vertices.begin(), agg->vertices.end());
-  sub_aggregates.push_back(agg);
-  // Merge the interface edge sets
-  for (auto edge : agg->interface_edges) {
-    if (this->interface_edges.get<0>().count(edge)) {
-      this->interface_edges.erase(edge);
-    }
-    else this->interface_edges.insert(edge);
-  }
-}
-
-AggregatePtr Merge(const AggregatePtr& agg1, const AggregatePtr& agg2) {
-  AggregatePtr agg = make_shared<Aggregate>();
-  agg->vertices.insert(agg1->vertices.begin(), agg1->vertices.end());
-  agg->vertices.insert(agg2->vertices.begin(), agg2->vertices.end());
-  agg->sub_aggregates.push_back(agg1);
-  agg->sub_aggregates.push_back(agg2);
-  // Merge the interface edge sets
-  for (auto edge : agg1->interface_edges)
-    if (!agg2->interface_edges.get<0>().count(edge))
-      agg->interface_edges.insert(edge);
-  for (auto edge : agg2->interface_edges)
-    if (!agg1->interface_edges.get<0>().count(edge))
-      agg->interface_edges.insert(edge);
-  return agg;
-}
 
 /* Assumption on the mtx file representing a matrix:
  * 1. Vertex numbers start from 1 instead of 0
@@ -45,7 +19,7 @@ Graph::Graph(const char* filename) {
   ifstream file(filename);
   string line;
   while (getline(file, line) && (line.empty() || line[0] == '%'));
-  unsigned nrows, ncols, nnz;
+  int nrows, ncols, nnz;
   istringstream iss(line);
   if (!(iss >> nrows >> ncols >> nnz)) {
     throw runtime_error("Error reading matrix sizes! Exiting...");
@@ -53,13 +27,8 @@ Graph::Graph(const char* filename) {
   if (nrows != ncols) {
     throw logic_error("Not square matrix! Exiting...");
   }
-  for (VertexIndex i = 0; i < nrows; ++i) {
-    AggregatePtr agg = make_shared<Aggregate>();
-    agg->AddVertex(i);
-    aggregates.push_back(agg);
-  }
-  adjacency_table.resize(nrows);
-  unsigned count = 0, i, j;
+  vector<vector<int>> adjacency_table(nrows);
+  int count = 0, edge_count = 0, i, j;
   while (getline(file, line)) {
     ++count;
     istringstream iss(line);
@@ -69,112 +38,43 @@ Graph::Graph(const char* filename) {
     if (i == j) continue;
     --i; --j;
     assert(adjacency_table[i].empty() || adjacency_table[i].back() < j);
-    adjacency_table[i].push_back(VertexIndex(j));
-    aggregates[i]->AddInterfaceEdge(Edge(i, j));
+    adjacency_table[i].push_back(j);
     assert(adjacency_table[j].empty() || adjacency_table[j].back() < i);
-    adjacency_table[j].push_back(VertexIndex(i));
-    aggregates[j]->AddInterfaceEdge(Edge(j, i));
-
-    interfaces.Add(i, j, Edge(i, j));
+    adjacency_table[j].push_back(i);
+    edge_count += 2;
   }
   assert(count == nnz);
 
-  size = nrows;
-  vertex_to_aggregate.resize(size);
-  GenerateEdges();
+  A = (iCSRmat *)malloc(sizeof(iCSRmat));
+  *A = icsr_create(nrows, ncols, edge_count);
+  int ind = 0;
+  for (int i = 0; i < nrows; ++i) {
+    A->IA[i] = ind;
+    for (auto j : adjacency_table[i]) {
+      A->JA[ind] = j;
+      A->val[ind] = 1;
+      ++ind;
+    }
+  }
+  A->IA[nrows] = ind;
+  assert(ind == A->nnz);
 
   cout << "Graph Reading Done..." << endl;
 }
 
-// Graph &Graph::operator=(const Graph &other) {
-//   size = other.size;
-//   adjacency_table = other.adjacency_table;
-//   vertex_to_aggregate = other.vertex_to_aggregate;
-//   edges = other.edges;
-//   edge_to_index = other.edge_to_index;
-//   aggregates = other.aggregates;
-//   interfaces = other.interfaces;
-//   return *this;
-// }
-
-void Graph::GenerateEdges() {
-  for (VertexIndex i = 0; i < adjacency_table.size(); ++i)
-    for (auto j : adjacency_table[i]) {
-      if (j > i) {
-        Edge e(i, j);
-        edges.push_back(e);
-        edge_to_index[e] = edges.size() - 1;
-      }
-    }
-}
-
-void Graph::GenerateVertexMap() {
-  for (AggregateIndex i = 0; i < aggregates.size(); ++i)
-    for (auto vi : aggregates[i]->vertices)
-      vertex_to_aggregate[vi] = i;
-}
-
-void Graph::UpdateVertexMap(const vector<AggregateIndex> &agg_indices) {
-  for (auto i : agg_indices)
-    for (auto vi : aggregates[i]->vertices)
-      vertex_to_aggregate[vi] = i;
-}
-
-void Graph::GenerateInterfaces(AggregateIndex i) {
-  for (auto edge : aggregates[i]->interface_edges) {
-    AggregateIndex j = vertex_to_aggregate[edge.tail];
-    interfaces.Add(i, j, edge);
-  }
-}
-
-// void Graph::GeneratePiHColumns(const AggregatePtr &agg) {
-//   for (auto edge : agg->interface_edges) {
-//     if (agg->PiHColumns.count(edge.head)) continue;
-//     unordered_set<VertexIndex> visited;
-//     visited.insert(edge.head);
-//     TreeSize(edge.head, agg, visited, agg->PiHColumns[edge.head]);
-//   }
-// }
-//
-// int Graph::TreeSize(VertexIndex root, const AggregatePtr &agg,
-//                     unordered_set<VertexIndex> &visited, EdgeVector &v) {
-//   vector<VertexIndex> children;
-//   for (auto nbr : adjacency_table[root])
-//     if (agg->HasVertex(nbr) && !visited.count(nbr)) {
-//       children.push_back(nbr);
-//       visited.insert(nbr);
-//     }
-//   int size = 0;
-//   for (auto child : children) {
-//     int subtreesize = TreeSize(child, agg, visited, v);
-//     Edge e(root, child);
-//     v.edge_indices.push_back(edge_to_index[e]);
-//     double sign = child < root ? 1.0 : -1.0;
-//     v.values.push_back(sign * (double)subtreesize / agg->Size());
-//     size += subtreesize;
-//   }
-//   return size + 1;
-// }
-
-void Graph::DoMatching() {
-  if (reshaped) {
-    throw runtime_error(
-        "The graph has been reshaped, no matching is performed!");
-    return;
-  }
-
-  size_t n = NumOfAggregates();
+void Graph::DoMatching(Graph *c_graph) {
+  int n = Size();
   if (n == 1) {
-    throw runtime_error("Only 1 aggregate, no matching is performed!");
+    throw runtime_error("Only 1 node, no matching is performed!");
     return;
   }
 
   // Table that stores the number of connections between aggregates
   struct ConnectivityEntry {
-    AggregateIndex nbr_index;
+    int nbr_index;
     int num_connections;
 
-    ConnectivityEntry(AggregateIndex i, int n):
+    ConnectivityEntry(int i, int n):
         nbr_index(i), num_connections(n) {}
   };
   struct GoBefore {
@@ -183,27 +83,32 @@ void Graph::DoMatching() {
       return e1.nbr_index < e2.nbr_index;
     }
   };
-  std::vector<std::vector<ConnectivityEntry> > connectivity_table(n);
 
-  for (auto interface : interfaces) {
-    AggregateIndex i = interface.head_index, j = interface.tail_index;
-    int size = interface.Size();
-    connectivity_table[i].push_back(ConnectivityEntry(j, size));;
-    connectivity_table[j].push_back(ConnectivityEntry(i, size));
+  vector<vector<ConnectivityEntry>> connectivity_table(n);
+  for (int i = 0; i < A->row; ++i) {
+    for (int ind = A->IA[i]; ind < A->IA[i+1]; ++ind) {
+      int j = A->JA[ind];
+      int w = A->val[ind];
+      connectivity_table[i].push_back(ConnectivityEntry(j, w));
+      connectivity_table[j].push_back(ConnectivityEntry(i, w));
+    }
   }
   for (auto &row : connectivity_table)
     sort(row.begin(), row.end(), GoBefore());
 
   vector<int> grouping_label(n, -1);
-  vector<pair<int, AggregateIndex> > degree_index(n);
-  for (AggregateIndex i = 0; i < n; ++i) {
-    degree_index[i] = make_pair(aggregates[i]->interface_edges.size(), i);
+  vector<pair<int, int>> degree_index(n);
+  for (int i = 0; i < n; ++i) {
+    int degree = 0;
+    for (auto entry : connectivity_table[i]) {
+      degree += entry.num_connections;
+    }
+    degree_index[i] = make_pair(degree, i);
   }
   sort(degree_index.begin(), degree_index.end());
-  vector<AggregatePtr> aggregates_new;
-  size_t count = 0;
+  int count = 0;
   for (auto p : degree_index) {
-    AggregateIndex i = p.second;
+    int i = p.second;
     if (grouping_label[i] != -1) continue;
     int max = 0, j = -1;
     for (auto entry : connectivity_table[i]) {
@@ -215,7 +120,7 @@ void Graph::DoMatching() {
     }
     // If all neighbors are already matched
     if (j == -1) {
-      unordered_map<AggregateIndex, int> num_connections_to;
+      unordered_map<int, int> num_connections_to;
       for (auto entry : connectivity_table[i]) {
         num_connections_to[grouping_label[entry.nbr_index]]
             += entry.num_connections;
@@ -233,216 +138,92 @@ void Graph::DoMatching() {
         exit(1);
       }
       grouping_label[i] = chosen.nbr_index;
-      aggregates_new[chosen.nbr_index]->Merge(aggregates[i]);
+      aggregates[chosen.nbr_index].push_back(i);
     }
     else {
-      aggregates_new.push_back(Merge(aggregates[i], aggregates[j]));
+      aggregates.push_back(vector<int>{i, j});
       grouping_label[i] = grouping_label[j] = count++;
     }
   }
-  aggregates = aggregates_new;
 
-  // Recompute the set of interfaces
-  InterfaceSet interfaces_new;
-  for (auto interface : interfaces) {
-    AggregateIndex ai = grouping_label[interface.head_index],
-                   aj = grouping_label[interface.tail_index];
-    if (ai != aj) interfaces_new.Merge(ai, aj, interface.edges);
+  vector<set<int>> c_adjacency_table(count);
+  for (int i = 0; i < A->row; ++i) {
+    for (int ind = A->IA[i]; ind < A->IA[i+1]; ++ind) {
+      int j = A->JA[ind];
+      int I = grouping_label[i];
+      int J = grouping_label[j];
+      if (I != J) {
+        c_adjacency_table[I].insert(J);
+      }
+    }
   }
-  interfaces = interfaces_new;
+  int nnz = 0;
+  for (auto nbs : c_adjacency_table) {
+    nnz += nbs.size();
+  }
+  iCSRmat *c_A = (iCSRmat *)malloc(sizeof(iCSRmat));
+  *c_A = icsr_create(count, count, nnz);
 
-  GenerateVertexMap();
+  int ind = 0;
+  for (int i = 0; i < count; ++i) {
+    c_A->IA[i] = ind;
+    for (auto nb : c_adjacency_table[i]) {
+      c_A->JA[ind] = nb;
+      c_A->val[ind] = 1;
+      ++ind;
+    }
+  }
+  c_A->IA[count] = ind;
+  assert(ind == nnz);
+
+  c_graph->A = c_A;
 }
 
-void Graph::DoMatching(Graph *c_graph) {
-  DoMatching();
-
-  auto size = aggregates.size();
-  for (VertexIndex i = 0; i < size; ++i) {
-    AggregatePtr agg = make_shared<Aggregate>();
-    agg->AddVertex(i);
-    c_graph->aggregates.push_back(agg);
-  }
-
-  c_graph->adjacency_table.resize(size);
-  for (auto interface : interfaces) {
-    AggregateIndex i = interface.head_index, j = interface.tail_index;
-    c_graph->adjacency_table[i].push_back(VertexIndex(j));
-    c_graph->aggregates[i]->AddInterfaceEdge(Edge(i, j));
-    c_graph->adjacency_table[j].push_back(VertexIndex(i));
-    c_graph->aggregates[j]->AddInterfaceEdge(Edge(j, i));
-    c_graph->interfaces.Add(i, j, Edge(i, j));
-  }
-
-  for (VertexIndex i = 0; i < size; ++i) {
-    sort(c_graph->adjacency_table[i].begin(), c_graph->adjacency_table[i].end());
-  }
-
-  c_graph->size = size;
-  c_graph->vertex_to_aggregate.resize(size);
-  c_graph->GenerateEdges();
-}
-
-// void Graph::SplitAggregates(const std::vector<AggregateIndex> &indices) {
-//   int size = indices.size();
-//   for (auto i : indices) {
-//     AggregatePtr a = aggregates[i];
-//     size_t size;
-//     while ((size = a->sub_aggregates.size()) == 1)
-//       a = a->sub_aggregates[0];
-//     if (size == 0) {
-//       cout << "Aggregate " << i << " is a vertex! Skipped" << endl;
-//       continue;
-//     }
-//
-//     vector<AggregateIndex> agg_indices(size);
-//     agg_indices[0] = i;
-//     for (size_t j = 1, offset = aggregates.size() - 1; j < size; ++j)
-//       agg_indices.push_back(offset + j);
-//
-//     interfaces.remove(i);
-//     aggregates[i] = a->sub_aggregates[0];
-//     aggregates.insert(aggregates.end(), a->sub_aggregates.begin()+1,
-//                       a->sub_aggregates.end());
-//
-//     UpdateVertexMap(agg_indices);
-//   }
-//
-//   // Generate new interfaces
-//   for (auto i : indices) GenerateInterfaces(i);
-//   for (size_t i = size; i < aggregates.size(); ++i) GenerateInterfaces(i);
-// }
-
-void Graph::GetAggregate(AggregateIndex i, std::vector<int> *vertices) const {
-  for (auto v : aggregates[i]->vertices) {
+void Graph::GetAggregate(int i, std::vector<int> *vertices) const {
+  for (auto v : aggregates[i]) {
     vertices->push_back(v);
   }
 }
 
 dCSRmat *Graph::GetWeightedLaplacian() const {
-  size_t n = NumOfAggregates();
+  int n = Size();
+  int nnz = A->nnz + n;
 
-  INT nnz = n;
-  for (AggregateIndex i = 0; i < n; ++i) {
-    nnz += aggregates[i]->interface_edges.size();
+  dCSRmat *L  = (dCSRmat *)malloc(sizeof(dCSRmat));
+  *L = dcsr_create(n, n, nnz);
+
+  int L_ind = 0;
+  for (int i = 0; i < n; ++i) {
+    L->IA[i] = L_ind;
+    REAL sum = 0;
+    int L_ind_diag;
+    int ind = A->IA[i];
+    while (ind < A->IA[i+1]) {
+      int j = A->JA[ind];
+      assert(j != i);
+      if (j > i) {
+        break;
+      }
+      L->JA[L_ind] = j;
+      L->val[L_ind] = -A->val[ind];
+      sum += A->val[ind];
+      ++L_ind;
+      ++ind;
+    }
+    L_ind_diag = L_ind++;
+    while (ind < A->IA[i+1]) {
+      int j = A->JA[ind];
+      assert(j != i);
+      L->JA[L_ind] = j;
+      L->val[L_ind] = -A->val[ind];
+      sum += A->val[ind];
+      ++L_ind;
+      ++ind;
+    }
+    L->JA[L_ind_diag] = i;
+    L->val[L_ind_diag] = sum;
   }
-
-  dCOOmat *B = (dCOOmat*)malloc(sizeof(dCOOmat));
-  B->row = n;
-  B->col = n;
-  B->nnz = nnz;
-  B->rowind = (INT*)malloc(sizeof(INT)*nnz);
-  B->colind = (INT*)malloc(sizeof(INT)*nnz);
-  B->val = (REAL*)malloc(sizeof(REAL)*nnz);
-
-  int ind = 0;
-  for (AggregateIndex i = 0; i < n; ++i) {
-    B->rowind[ind] = i;
-    B->colind[ind] = i;
-    B->val[ind] = (REAL)(aggregates[i]->interface_edges.size());
-    ++ind;
-  }
-  for (auto interface : interfaces) {
-    AggregateIndex i = interface.head_index, j = interface.tail_index;
-    B->rowind[ind] = i;
-    B->colind[ind] = j;
-    B->val[ind] = -(REAL)interface.Size();
-    ++ind;
-    B->rowind[ind] = j;
-    B->colind[ind] = i;
-    B->val[ind] = -(REAL)interface.Size();
-    ++ind;
-  }
-
-  dCSRmat *A = (dCSRmat*)malloc(sizeof(dCSRmat));
-  dcoo_2_dcsr(B, A);
-  free(B);
-
-  return A;
+  L->IA[n] = L_ind;
+  assert(L_ind == nnz);
+  return L;
 }
-
-// mfem::Vector Graph::GetCoarseRHS(const mfem::Vector &f) const {
-//   assert(f.Size() == size);
-//   size_t n = NumOfAggregates();
-//   mfem::Vector fh(n);
-//   for (AggregateIndex i = 0; i < n; ++i) {
-//     fh(i) = 0;
-//     for (VertexIndex vi : aggregates[i]->vertices)
-//       fh(i) += f(vi);
-//   }
-//   return fh;
-// }
-//
-// mfem::Vector Graph::Prolongate(const mfem::Vector &uh_c) const {
-//   size_t n = Size(), nc = NumOfAggregates();
-//   mfem::Vector uh(n);
-//   for (AggregateIndex i = 0; i < nc; ++i)
-//     for (VertexIndex vi : aggregates[i]->vertices)
-//       uh(vi) = uh_c(i);
-//   return uh;
-// }
-//
-// mfem::SparseMatrix *Graph::GetCoarseEdgeSpaceBasis() {
-//   assert(edge_to_index.size() == edges.size());
-//   size_t ne = edge_to_index.size(), nec = interfaces.size();
-//   mfem::SparseMatrix Phi_t(nec, ne);
-//   int row = 0;
-//   for (auto interface : interfaces) {
-//     AggregatePtr head_agg = aggregates[interface.head_index],
-//                  tail_agg = aggregates[interface.tail_index];
-//     if (!head_agg->PiHGenerated()) GeneratePiHColumns(head_agg);
-//     if (!tail_agg->PiHGenerated()) GeneratePiHColumns(tail_agg);
-//     for (auto edge : interface.edges) {
-//       VertexIndex head = edge.head, tail = edge.tail;
-//       if (!head_agg->HasVertex(head)) swap(head, tail);
-//       mfem::Array<int> cols1;
-//       toArray(head_agg->PiHColumns[head].edge_indices, cols1);
-//       Phi_t.AddRow(row, cols1, toVector(head_agg->PiHColumns[head].values));
-//       mfem::Array<int> cols2;
-//       toArray(tail_agg->PiHColumns[tail].edge_indices, cols2);
-//       mfem::Vector values = toVector(tail_agg->PiHColumns[tail].values);
-//       Phi_t.AddRow(-row-1, cols2, values);
-//
-//       Phi_t.Set(row, edge_to_index[edge], head < tail ? 1.0 : -1.0);
-//     }
-//     ++row;
-//   }
-//   Phi_t.Finalize();
-//   Phi_t.SortColumnIndices();
-//   return Transpose(Phi_t);
-// }
-//
-// mfem::Vector Graph::GetLocalEstimates(mfem::Vector f, const mfem::Vector &uh,
-//                                       mfem::Vector y, double CP) const {
-//   G->AddMult(uh, y, -1.0);
-//   G->AddMultTranspose(y, f, -1.0);
-//   size_t nc = NumOfAggregates();
-//   mfem::Vector local_est(nc);
-//   local_est = 0.0;
-//   // Vertices
-//   for (AggregateIndex i = 0; i < nc; ++i)
-//     for (VertexIndex vi : aggregates[i]->vertices)
-//       local_est(i) += f(vi) * f(vi) / CP / CP;
-//   // Interface edges
-//   for (auto interface : interfaces) {
-//     AggregateIndex i = interface.head_index, j = interface.tail_index;
-//     for (auto edge : interface.edges) {
-//       auto it = edge_to_index.find(edge);
-//       assert(it != edge_to_index.end());
-//       EdgeIndex ei = it->second;
-//       double v = 0.5 * y(ei) * y(ei);
-//       local_est(i) += v;
-//       local_est(j) += v;
-//     }
-//   }
-//   // Interior edges
-//   for (auto pair : edge_to_index) {
-//     const Edge &edge = pair.first;
-//     EdgeIndex ei = pair.second;
-//     VertexIndex vi = edge.head, vj = edge.tail;
-//     AggregateIndex i;
-//     if ((i = vertex_to_aggregate[vi]) == vertex_to_aggregate[vj])
-//        local_est(i) += y(ei) * y(ei);
-//   }
-//   return local_est;
-// }
