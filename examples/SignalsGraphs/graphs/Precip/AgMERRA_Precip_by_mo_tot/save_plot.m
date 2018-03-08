6a79e67196a0d424d88207365d10d7363b7d1d65
Wenfang Xu <wxu@mathcalc.math.psu.edu>
2018-03-08 00:17:29 -0500

    made matlab generate borderless images
    Former-commit-id: a25f05bab4ea80bd256ff2baf0c319699421b68e [formerly 46e80afe7bfbebcb7b583b6c10a0187c9dca0b63]
    Former-commit-id: b3253fc53dd08d4af07f1323940785a414ce7b92

--- examples/SignalsGraphs/graphs/Precip/AgMERRA_Precip_by_mo_tot/save_plot.m
+++ examples/SignalsGraphs/graphs/Precip/AgMERRA_Precip_by_mo_tot/save_plot.m
@@ -3,7 +3,6 @@ for i = 1:length(files)
   filename = files(i).name;
   if length(filename) >= 3 & filename(1:3) == 'agp'
     data = dlmread(filename);
-    im = imagesc(data);
-    saveas(im, strcat('images/',filename,'.png'), 'png');
+    imwrite(ind2rgb(im2uint8(mat2gray(data)), parula(256)), strcat('images/',filename,'.png'), 'png');
   end
 end
