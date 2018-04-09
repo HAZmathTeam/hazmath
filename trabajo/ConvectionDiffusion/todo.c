aa5c0c23bb009580b9305b56604214756a2bee48
Ludmil Zikatanov <ludmil@zsolvers.net>
2018-04-09 16:39:53 -0400

    ConvectionDiffusion simple local matrices
    Former-commit-id: fdff1fbcdca217ef64f1034b8344564ce4c02b90 [formerly 7cc46b185f0dca4db19f6abc36dba72702c80983]
    Former-commit-id: 9fd4bc7604d8041a49fdd258b5d47a17e6be88ba

--- trabajo/ConvectionDiffusion/todo.c
+++ trabajo/ConvectionDiffusion/todo.c
@@ -1,62 +1,42 @@
 
 /**************************************************/
-// tangent vectors are the columns of bs[].  then we take i and j
-// always in one and the same direction by doing i<j or things like
-// that...
-    iaa=ia[i]-1;
-    iab=ia[i+1]-1;
-    for (jk=iaa; jk<iab; jk++){
-      j=ja[jk]-1;
-      /*      fprintf(stdout,"aaaaa %i,%i\n",i,j); */
-      if(i != j){
-	xj=xyz->x[j];
-	yj=xyz->y[j];
-	if(dim>2) 
-	  zj=xyz->z[j];
-	/* 
-	   compute the advection field at the middle of the edge and
-	   then the bernoulli function
-	*/
-	te[0]  = xi - xj;
-	te[1]  = yi - yj;
-	xm[0] = (xi + xj)*0.5e+0;
-	xm[1] = (yi + yj)*0.5e+0;
-	if(dim>2){
-	  te[2]  = zi - zj;
-	  xm[2] = (zi + zj)*0.5e+0;
-	}
-	vector_val_ad(ad,xm,0.0,&edge_flag);
-	bte=0.;
-	for(jdim=0;jdim<dim;jdim++){
-	  bte += ad[jdim]*te[jdim];
-	}
-	/* alpe=a(xmid)\approx harmonic_average=|e|/(int_e 1/a); */
-	/* should be computed by quadrature in general for 1/a(x). */
-	/* a_{ij}=B(beta.t_e/harmonic_average)*harmonic_average*omega_e; */
-	/* 	  for (i,j):    B(beta.t_e/harmonic_average);    */
-	/* 	  for (j,i): B(-beta.t_e/harmonic_average). */
-	/* 
-	   This is direction independent, because t_e=xi-xj; If places
-	   of i and j are switched, then we are working on a_{ji} with
-	   the t_e of opposite sign.
-	 */	
-	scalar_val_d(&alpe,xm,0.0,&edge_flag);
-	a[jk] *= (alpe*(bernoulli(bte/alpe))); 
-	//	fprintf(stdout,"\ndd=%22.14e",(alpe*(bernoulli(bte/alpe))));
-	/* the diagonal is equal to the negative column sum;*/
+for(i=0;i<dim1;i++){
+  id=i*dim; // this is for xs, so only dim columns
+  for(j=0;j<dim1;j++){
+    if(i==j) continue;
+    /* \beta\cdot \tau_e */
+    ij=i*dim1+j; // this is for stiff local.
+    jd=j*dim; // this is for xs, so only dim columns
+    bte=0.; 
+    if(nop(j)>nop(i))
+      for(k=0;k<dim;k++) bte+=ad[k]*(xs[jd+k]-xs[id+k]);
+    else if(nop(j)!=nop(i)) // reverse tau
+      for(k=0;k<dim;k++) bte+=ad[k]*(xs[id+k]-xs[jd+k]);
+      //old for the above... for(k=0;k<dim;k++) te[k]=xs[jd+k]-xs[id+k];
+      //old for the above... for(k=0;k<dim;k++) te[k]=xs[id+k]-xs[jd+k];
+    /*midpoints*/
+    // mid points on the edges if needed!    for(k=0;k<dim;k++) xm[k]=0.5*(xs[jd+k]+xs[id+k]);
+    aloc[ij] *= bernoulli(bte/alpe); 
+    //	fprintf(stdout,"\ndd=%22.14e",(alpe*(bernoulli(bte/alpe))));
+    /* the diagonal is equal to the negative column sum;*/    
+    d[j]-=aloc[ij];
+  }
+ }
+/* Another loop to set up the diagonal equal to whatever it needs to
+   equal. */
+for (i = 0; i < dim1; i++)
+  aloc[i+dim1+i]=diag0.val[i];
 
-	diag0.val[j]-=a[jk];
-      }
-    } 
-  }
-  /* Another loop to set up the diagonal equal to whatever it needs to
-     equal. */
-  for (i = 0; i < nv; i++){
-    iaa=ia[i]-1;
-    iab=ia[i+1]-1;
-    for (jk=iaa; jk<iab; jk++){
-      j = ja[jk]-1; 
-      if(i == j) a[jk]=diag0.val[i];
-    }
-  }
+//	vector_val_ad(ad,xm,0.0,&edge_flag);
+/* alpe=a(xmid)\approx harmonic_average=|e|/(int_e 1/a); */
+/* should be computed by quadrature in general for 1/a(x). */
+/* a_{ij}=B(beta.t_e/harmonic_average)*harmonic_average*omega_e; */
+/* 	  for (i,j):    B(beta.t_e/harmonic_average);    */
+/* 	  for (j,i): B(-beta.t_e/harmonic_average). */
+/* 
+   This is direction independent, because t_e=xi-xj; If places
+   of i and j are switched, then we are working on a_{ji} with
+   the t_e of opposite sign.
+*/	
+/* scalar_val_d(&alpe,xm,0.0,&edge_flag); */
   /*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
