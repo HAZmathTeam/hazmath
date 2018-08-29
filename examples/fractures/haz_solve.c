42d25bafffd0bbcadf0d74516a091d22067d9deb
Ludmil T Zikatanov <ludmil02@gmail.com>
2018-08-29 20:34:12 +0200

    fractures test case
    Former-commit-id: cf29178172399409d3ece9a91cfe03256fe7061d

---
+++ examples/fractures/haz_solve.c
@@ -0,0 +1,78 @@
+/*! \file src/utilities/wrapper.c
+ *
+ *  Created by James Adler, Xiaozhe Hu, and Ludmil Zikatanov on 09/02/16.
+ *  Copyright 2016__HAZMATH__. All rights reserved.
+ *
+ *  \note: modified by ltz1 on 08/29/2018
+ *  \note: modified by Xiaozhe Hu on 10/27/2016
+ *  \note: done cleanup for releasing -- Xiaozhe Hu 10/27/2016
+ *
+ */
+
+#include "hazmath.h"
+INT main(int *argc, char **argv) {
+    dCOOmat         *Acoo;      // matrix
+    dCSRmat         *Acsr;      // matrix
+    dvector         *rhs, *sol; // right-hand-side, solution
+    /***************************************************/
+    AMG_param       amgparam; // parameters for AMG
+    linear_itsolver_param  itparam;  // parameters for linear itsolver    
+    input_param inparam;
+    INT i;
+    INT print_lvl=4;
+    INT maxit=1000;
+    REAL tol=1e-10;
+    /***************************************************/
+    Acoo=(dCOOmat *)malloc(sizeof(dCOOmat));
+    FILE *fin = HAZ_fopen("matrix.ijv","r");
+    i=fscanf(fin,"%i",&(Acoo->row));
+    i=fscanf(fin,"%i",&(Acoo->col));
+    i=fscanf(fin,"%i",&(Acoo->nnz));
+    Acoo->rowind=calloc(Acoo->nnz,sizeof(INT));
+    Acoo->colind=calloc(Acoo->nnz,sizeof(INT));
+    Acoo->val=calloc(Acoo->nnz,sizeof(REAL));
+    for(i=0;i<Acoo->nnz;i++){
+      fscanf(fin,"%i %i %lg",(Acoo->rowind+i),(Acoo->colind+i),(Acoo->val+i));
+      Acoo->rowind[i]--;Acoo->colind[i]--;
+      //      fprintf(stdout,"\n%i: %i %i %23.16e",i,Acoo->rowind[i],Acoo->colind[i],Acoo->val[i]);
+    }
+    fclose(fin);
+    fprintf(stdout,"\n");
+    Acsr=(dCSRmat *)malloc(sizeof(dCSRmat));
+    Acsr->row=Acoo->row;
+    Acsr->col=Acoo->col;
+    dcoo_2_dcsr(Acoo,Acsr);
+    //    FILE *fid = HAZ_fopen("matrix.nnn","w");
+    //    csr_print_matlab(fid,Acsr);
+    //    fclose(fid);
+    //    exit(255);
+    free(Acoo->val);free(Acoo->rowind);free(Acoo->colind);free(Acoo);
+    rhs=(dvector *)malloc(sizeof(dvector));
+    sol=(dvector *)malloc(sizeof(dvector));
+    rhs->row = Acsr->row; rhs->val = calloc(Acsr->col,sizeof(REAL));
+    for(i=0;i<rhs->row;i++){rhs->val[i]=11.e00;}
+    sol->row = Acsr->col; sol->val = calloc(Acsr->row,sizeof(REAL));
+    //
+    param_input_init(&inparam);
+    param_input("./input.dat", &inparam);
+    
+    // Set parameters for linear iterative methods
+    param_linear_solver_init(&itparam);
+    param_linear_solver_set(&itparam, &inparam);
+    if (print_lvl > PRINT_MIN) param_linear_solver_print(&itparam);
+    
+    // Set parameters for algebriac multigrid methods
+    param_amg_init(&amgparam);
+    param_amg_set(&amgparam, &inparam);
+    if (print_lvl > PRINT_MIN) param_amg_print(&amgparam);
+        
+    amgparam.print_level          = print_lvl;
+    itparam.linear_tol            = tol;
+    itparam.linear_print_level    = print_lvl;
+    itparam.linear_maxit          = maxit;
+    /************************************************************/    
+    linear_solver_dcsr_krylov_amg(Acsr, rhs, sol, &itparam, &amgparam);
+    //    directsolve_UMF(&Acsr, &rhs, &sol, *print_lvl);
+}
+/***************************** END ***************************************************/
+
