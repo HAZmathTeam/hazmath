efacb7714b5f6ff6d48ecc25730de072ac70c387
Ludmil T Zikatanov <ludmil02@gmail.com>
2019-01-15 10:04:28 -0500

    amr utilities
    Former-commit-id: 6e2e5cb7e0df83b17f6e326c3bf02b53cbf3b6e9

---
+++ src/utilities/amr_utils.c
@@ -0,0 +1,120 @@
+/*! \file src/utilities/amr_utils.c
+ *
+ *  Created by James Adler, Xiaozhe Hu, and Ludmil Zikatanov on 20190115.
+ *  Copyright 2017__HAZMATH__. All rights reserved.
+ *
+ *  \note contains some utility functions for mesh refinement. 
+ *
+ */
+#include "hazmath.h"
+INT xins(INT n, INT *nodes, REAL *xs, REAL *xstar)
+{
+  /* 
+     In dimension "n" constructs the map from reference simplex to
+     simplex with coordinates xs[0..n].  Then solves a linear system
+     with partial pivoting to determine if a point given with
+     coordinates xstar[0..n-1] is in the (closed) simplex defined by
+     "nodes[0..n] and xs[0..n]"
+  */  
+  INT n1=n+1,i,j,l0n,ln,j1;
+  INT *p=NULL;
+  REAL *A=NULL,*xhat=NULL, *piv=NULL;
+  A=(REAL *)calloc(n*n,sizeof(REAL));
+  xhat=(REAL *)calloc(n,sizeof(REAL));
+  piv=(REAL *)calloc(n,sizeof(REAL));
+  p=(INT *)calloc(n,sizeof(INT));
+  /* fprintf(stdout,"\nj=%d; vertex=%d\n",0+1,nodes[0]+1); fflush(stdout); */
+  /* fprintf(stdout,"\nvertex=%d\n",nodes[0]+1); */
+  /* for(i=0;i<n;i++){ */
+  /*   fprintf(stdout,"xyz=%f ",xs[l0n+i]); */
+  /* } */
+  /* fprintf(stdout,"\n"); */
+  l0n=nodes[0]*n;
+  for (j = 1; j<n1;j++){
+    /* grab the vertex */
+    ln=nodes[j]*n;
+    j1=j-1;
+    for(i=0;i<n;i++){
+      /*A_{ij} = x_{i,nodes(j)}-x0_{i,nodes(j)},j=1:n (skip 0)*/
+      A[i*n+j1] = xs[ln+i]-xs[l0n+i];
+    }
+  }
+  //  fflush(stdout);
+  for(i=0;i<n;i++){
+    xhat[i] = xstar[i]-xs[l0n+i];
+  }
+  //print_full_mat(n,1,xstar,"xstar");
+  //  print_full_mat(n,n,A,"A");
+  //  print_full_mat(n,1,xhat,"xhat0");
+  solve_pivot(1, n, A, xhat, p, piv);
+  //  print_full_mat(n,1,xhat,"xhat");
+  //  fprintf(stdout,"\nSolution:\n");
+  //  print_full_mat(n,1,xhat,"xhat");
+  REAL xhatn=1e0,eps0=1e-10,xmax=1e0+eps0;
+  /* check the solution if within bounds */
+  INT flag = 0;
+  for(j=0;j<n;j++){
+    if((xhat[j] < -eps0) || (xhat[j] > xmax)){
+      flag=(j+1);
+      //      fprintf(stdout,"\nNOT FOUND: xhat(%d)=%e\n\n",flag,xhat[j]);
+      break;
+    }
+    xhatn -= xhat[j];
+    if((xhatn<-eps0) || (xhatn>xmax)) {
+      flag=n+1;
+      //          fprintf(stdout,"\nNOT FOUND: xhat(%d)=%e\n\n",flag,xhatn);
+      break;
+    }
+  }
+  if(A) free(A);
+  if(xhat) free(xhat);
+  if(p) free(p);
+  if(piv) free(piv);
+  return flag;
+}
+void marks(scomplex *sc,dvector *errors)
+{
+  /* mark simplices depending on the value of an estimator */
+  /* the estimator here is the aspect ratio of the simplex */
+  INT n=sc->n,n1=n+1,ns=sc->ns,nv=sc->nv,level=sc->level;
+  INT kbadel,ke,i,j,j1,k,p,q,ni,node1,node2,mj,mk;
+  INT ne=(INT )((n*n1)/2);
+  REAL slmin,slmax,asp,aspmax=-10.;;
+  REAL *sl=(REAL *)calloc(ne,sizeof(REAL));
+  INT kbad=0;
+  for(i = 0;i<ns;i++){
+    if(sc->gen[i] < level) continue;
+    ni=n1*i;
+    ke=0;
+    for (j = 0; j<n;j++){      
+      mj=sc->nodes[ni+j]*n;
+      j1=j+1;
+      for (k = j1; k<n1;k++){
+	mk=sc->nodes[ni+k]*n;
+	sl[ke]=0e0;
+	for(p=0;p<n;p++){
+	  sl[ke]+=(sc->x[mj+p]-sc->x[mk+p])*(sc->x[mj+p]-sc->x[mk+p]);
+	}
+	sl[ke]=sqrt(sl[ke]);
+	ke++;
+      }
+    }
+    slmin=sl[0];
+    slmax=sl[0];
+    for(j=1;j<ne;j++){
+      if(sl[j]<slmin) slmin=sl[j];
+      if(sl[j]>slmax) slmax=sl[j];
+    }
+    asp=slmax/slmin;
+    if(asp>1e1){
+      sc->marked[i]=1;
+      kbad++;
+      //      fprintf(stdout,"\nlev=%d, gen=%d, asp= %e(%e/%e)\n",level,sc->gen[i],asp,slmax,slmin);
+      if(asp>aspmax){kbadel=i;aspmax=asp;}
+    }
+  }
+  //  fprintf(stdout,"\nbad:%d, aspectmax=%e (at el=%d)\n",kbad,aspmax,kbadel);
+  //  exit(33);
+  if(sl)free(sl);
+  return;
+}
