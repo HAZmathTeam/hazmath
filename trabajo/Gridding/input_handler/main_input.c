70c1c537671c17c938e41182eaec0ee0843946aa
Ludmil T Zikatanov <ludmil02@gmail.com>
2019-04-20 21:08:09 -0400

    added grid_input example in HAZMATH/trabajo/gridding/input_handler
    Former-commit-id: 8fdf0d00c7a1e778efff34c3b2a48a326a60a4a4 [formerly 8fab99e785d0568e29973041c16884085487210f]
    Former-commit-id: 2a17b0f5b3e503d869bc2c7c36a1eb422b47d5c2

---
+++ trabajo/Gridding/input_handler/main_input.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <float.h>
+#include <limits.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include <assert.h>
+#include "hazmath.h"
+/**********************************************************************/
+void input_grid_free(input_grid *g);
+void input_grid_print(input_grid *g);
+void coo2csr(INT nrow,INT ncol,INT nnz,					\
+	     INT *row_idx,INT *col_idx, void *aval,			\
+	     INT *ia,INT *ja, void *bval,				\
+	     size_t elsize);
+char **splits(char *s, const char *d, INT *num);
+void read_data(char *data_coordsystems,		\
+	       char *data_vertices,		\
+	       char *data_edges,		\
+	       input_grid *g);
+void get_out(char *pattern, size_t le);
+char *make_string_from_file(FILE *the_file, size_t *length_string);
+char *get_substring(char *pattern,		\
+		    size_t *length_substring,	\
+		    char *the_string);
+input_grid *parse_input_grid(const char *input_file_grid);
+/********************************************************************/
+int main(int argc, char **argv){
+  char input_grid_file[256]={"input.grid"};
+  input_grid *g=parse_input_grid(input_grid_file);
+  input_grid_print(g);  
+  input_grid_free(g);
+  return 0;
+}
