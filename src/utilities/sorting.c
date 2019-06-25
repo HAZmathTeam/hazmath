755e0bc462b782284bba1c178846d2509a8ade99
Ludmil T Zikatanov <ludmil02@gmail.com>
2019-06-25 06:58:34 +0200

    uniform grid example almost ready; hopefully will be finished on the flight to IAD
    Former-commit-id: 8eef97f65b4483acc4e470ceb8780c15b3669c92 [formerly d5daf4583e2e7cfeb5eeb4a0827776e51996f463]
    Former-commit-id: e4b73a7b0519ce71a2a2df86df5d6ec16a46208d

--- src/utilities/sorting.c
+++ src/utilities/sorting.c
@@ -7,6 +7,99 @@
  */
 /********************************************************************/
 #include "hazmath.h"
+/***************************************************************/
+void dlexsort(const INT nr, const INT nc,REAL *a,INT *p)
+{
+  /* REAL NUMBERS; identical below for ints. It can be combined later
+     --ltz 20190625
+     *
+     implements STRAIGHT INSERT sorting to order lexicographically nr
+     names with nc components each.  the array a is overwritten.  aj is
+     a working double array with nc (this will be dim in our most
+     common case) elements. used to sort coordinates of the vertices of
+     the macroelements lexicographically.  on output, a is ordered, p
+     is the permutation used to order a. The ogiginal a is recovered
+     with inv permutation aorig[]=a[invp[i]] where invp[p[i]]=i;
+  */
+  INT i,j,k,k1,pj;
+  unsigned int lt=0;
+  REAL *aj=(REAL *)calloc(nc,sizeof(REAL));
+  for (i = 0; i < nr; i++){p[i]=i;} 
+  for (j = 1; j < nr; ++j) {
+    //    for(k=0;k<nc;k++)aj[k] = a[j*nc+k];
+    memcpy(aj,(a+j*nc),nc*sizeof(REAL)); pj = *(p + j);
+    i = j - 1;
+    lt=0;
+    for(k=0;k<nc;k++) {
+      if(aj[k]>a[i*nc+k]){lt=0;break;}
+      else if(aj[k]<a[i*nc+k]){lt=1;break;}
+      else {continue;}	
+    }
+    //    while ((i >=0) && (aj<a[i])){
+    while ((i>=0) && (lt)){
+      memcpy((a+(i+1)*nc),(a+i*nc),nc*sizeof(REAL)); *(p+i+1)=*(p+i);
+      //      for(k=0;k<nc;k++) a[(i + 1)*nc+k] = a[i*nc+k];
+      --i;
+      if(i<0) break;
+      lt=0;
+      for(k=0;k<nc;k++) {
+	if(aj[k]>a[i*nc+k]){lt=0;break;}
+	else if(aj[k]<a[i*nc+k]){lt=1;break;}
+	else{continue;}
+      }
+    }
+    memcpy((a+(i+1)*nc),aj,nc*sizeof(REAL)); *(p+i+1)=pj;
+  }
+  if(aj) free(aj);
+  return;
+}
+/*********************************************************************/
+void ilexsort(const INT nr, const INT nc,INT *a,INT *p)
+{
+  /* INT NUMBERS; identical above for reals. It can be combined later into one.
+     --ltz 20190625
+     *
+     implements STRAIGHT INSERT sorting to order lexicographically nr
+     names with nc components each.  the array a is overwritten.  aj is
+     a working INT array with nc (this will be dim in our most
+     common case) elements. used to sort coordinates of the vertices of
+     the macroelements lexicographically.  on output, a is ordered, p
+     is the permutation used to order a. The ogiginal a is recovered
+     with inv permutation aorig[]=a[invp[i]] where invp[p[i]]=i;
+  */
+  INT i,j,k,k1,pj;
+  unsigned int lt=0;
+  INT *aj=(INT *)calloc(nc,sizeof(INT));
+  for (i = 0; i < nr; i++){p[i]=i;} 
+  for (j = 1; j < nr; ++j) {
+    //    for(k=0;k<nc;k++)aj[k] = a[j*nc+k];
+    memcpy(aj,(a+j*nc),nc*sizeof(INT)); pj = *(p + j);
+    i = j - 1;
+    lt=0;
+    for(k=0;k<nc;k++) {
+      if(aj[k]>a[i*nc+k]){lt=0;break;}
+      else if(aj[k]<a[i*nc+k]){lt=1;break;}
+      else {continue;}	
+    }
+    //    while ((i >=0) && (aj<a[i])){
+    while ((i>=0) && (lt)){
+      memcpy((a+(i+1)*nc),(a+i*nc),nc*sizeof(INT)); *(p+i+1)=*(p+i);
+      //      for(k=0;k<nc;k++) a[(i + 1)*nc+k] = a[i*nc+k];
+      --i;
+      if(i<0) break;
+      lt=0;
+      for(k=0;k<nc;k++) {
+	if(aj[k]>a[i*nc+k]){lt=0;break;}
+	else if(aj[k]<a[i*nc+k]){lt=1;break;}
+	else{continue;}
+      }
+    }
+    memcpy((a+(i+1)*nc),aj,nc*sizeof(INT)); *(p+i+1)=pj;
+  }
+  if(aj) free(aj);
+  return;
+}
+/***************************************************************/
 /*STRAIGHT INSERT sort */
 void isi_sort(INT n, INT *a)
 {
