7be10c10796b0ecda1b1cafae152f3693732b7ae
Ludmil T Zikatanov <ludmil02@gmail.com>
2019-07-07 09:36:28 -0400

    reading more than 1 macroelement in input file for grids
    Former-commit-id: b5d30129b56383f453498c941da3acd8faf4ad62

--- trabajo/gridding/header_input.h
+++ trabajo/gridding/header_input.h
@@ -12,6 +12,7 @@
 #include <unistd.h>
 #include <assert.h>
 
+
 #define PI 3.14159265358979323851281e+00
 #define EXP_OF_1  2.71828182845904523542817e+00
 
@@ -49,6 +50,29 @@
 #define uint unsigned int
 #endif
 /*================================================================*/
+#ifndef INPUT_GRID_DATA_
+#define INPUT_GRID_DATA_ "title{",		\
+    "dir_grid{",				\
+    "dir_vtu{",					\
+    "file_grid{",				\
+    "file_vtu{",				\
+    "data_coordsystems{",			\
+    "data_vertices{",				\
+    "data_edges{",				\
+    "data_macroelements{",			\
+    "data_macrofaces{",				\
+    "dimension{",				\
+    "num_coordsystems{",			\
+    "num_vertices{",				\
+    "num_edges{",				\
+    "num_macroelements{",			\
+    "num_macrofaces{",				\
+    "num_refinements{",				\
+    "refinement_type{",				\
+    "err_stop_refinement{",			\
+    "print_level{"
+#endif
+/*****************************************************************/
 typedef struct {
   char *title; // the title of the input
   INT dim; // the dimension of the problem.
@@ -71,22 +95,27 @@ typedef struct {
 	     computational domain */
   REAL *xv; /* coordinates for each vertex [nv][dim]*/
   INT *csysv; /* coordinate system labels for vertices [nv]*/
+  INT *labelsv; /* coordinate system labels for vertices [nv]*/
   INT *bcodesv; /* boundary codes for vertices [nv]*/
   INT ne; /* number of edges/segments */ 
   REAL *xe; /* coordinates for each midpoint of an edge [ne][dim]*/
   INT *seg;/* segments array of size ne by 3. For every edge:
 	      (v1,v2,divisions) with v1<v2 */
-  INT nmacro;/*number of macroelements*/
-  INT *macroel; /* macroelements: macroelement label, vertices forming
+  INT nel;/*number of macroelements*/
+  INT *mnodes; /* macroelements: macroelement label, vertices forming
 		   a macro element, macroelement material */ 
-  INT nmacrofaces;  /*number of macroelement faces that are marked
-		      with codes; boundary or internal it does not
-		      matter */
-  INT *macrofaces;   /* faces and boundary codes of faces */ 
+  INT nf;  /*number of macroelement faces that are marked
+	     with codes; boundary or internal it does not
+	     matter */
+  INT *mfaces;   /* faces and boundary codes of faces */ 
+  INT nref;   /* number of refinements (AMR)*/
+  INT ref_type;   /* refinement type -2,-1,0,1,2,3,4,... */ 
+  REAL err_stop;   /* stop tolerance for AMR */ 
 }input_grid; /** Input GRID parameters */
-/**********************************************************************/
+
 void ilexsort(const INT nr, const INT nc,INT *a,INT *p);
 /* void dlexsort(const INT nr, const INT nc,REAL *a,INT *p); */
 /* void isi_sort(INT n, INT *a); */
 /* void isi_sortp(const INT n, INT *a, INT *p, INT *invp); */
 /* void dsi_sortp(const INT n, REAL *a, INT *p, INT *invp); */
+
