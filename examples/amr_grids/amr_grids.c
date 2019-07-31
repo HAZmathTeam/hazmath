560b4e0096d76e1810f4b290fa73043e910feb82
Ludmil T Zikatanov <ludmil02@gmail.com>
2019-07-31 08:30:55 -0400

    added comments in amr_grids.c
    Former-commit-id: bbd3861192576b63da2277977fb73dce8ed3181f [formerly 35ba3a6cecf89da1a262515316b0092081d820ff]
    Former-commit-id: 3eb3cf7838f4a2e38a09165bd79769d2a1c7788b

--- examples/amr_grids/amr_grids.c
+++ examples/amr_grids/amr_grids.c
@@ -1,20 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <limits.h>
-#include <time.h>
+/*! \file examples/basic_elliptic/basic_elliptic.c
+ *
+ *  Created by James Adler, Xiaozhe Hu, and Ludmil Zikatanov 2019/01/09.
+ *  Copyright 2015_HAZMATH__. All rights reserved.
+ *
+ * \brief This program generates simplicial grids in 2,3,4... dimension. 
+ *
+ * \note This example highlights some of the features of the simple
+ * mesh generator included with HAZmath. It is only to illustrate how
+ * to use the mesh refinement. 
+ */
 
+/*********** HAZMATH FUNCTIONS and INCLUDES ***************************/
 #include "hazmath.h"
+/*********************************************************************/
 #include "solve_estimate_mark.h"
+/*********************************************************************/
 INT main(INT   argc,   char *argv[])
 {
   FILE *fp=stdin;     
-  //  fp=HAZ_fopen("2d_square.input","r"); 
+  //  fp=HAZ_fopen("2d_square.input","r");
+  /*
+    PARSE THE INPUT.
+  */
   input_grid *g=parse_input_grid(fp);
-  fclose(fp);
-  //    input_grid_print(g);
-  //    exit(44);
+  fclose(fp);  
+  /*
+    GENERATE INITIAL GRID AND DECLARE VARIABLES.
+  */
   scomplex *sc=generate_initial_grid(g);
   scomplex *sctop=NULL;
   INT ref_levels=g->nref, amr_marking_type=g->mark_type,j; 
@@ -22,12 +34,17 @@ INT main(INT   argc,   char *argv[])
   ivector *marked=NULL;
   void *all=NULL;
   if(amr_marking_type){
-    // all here can pass data around. so we make it 4 dvectors worth and one ivector (as an example)
+    /* 
+       Use "all" here can pass data around. Below we make 4 dvectors
+       and one ivector, just as example. A good example for using the
+       array will be to pass the whole hierarchy, not only the fine
+       grid via all, e.g. all=(coid *)sc
+    */
     all=(void *)malloc(5*sizeof(dvector)+sizeof(ivector)); 
     /**/    
     for(j=0;j<ref_levels;j++){
       /*
-       * select the finest level: 
+       * SELECT the finest grid:
        */
       sctop=scfinest(sc); 
       /* 
@@ -42,11 +59,12 @@ INT main(INT   argc,   char *argv[])
        * MARK: marked is an ivector with num.rows=the number of
        *       simplices; its componenets are nonzero if the simplex
        *       is marked for refinement and 0 if it is not marked for
-       *       refinement
+       *       refinement.
        */
       marked=exmpl_mark(sctop,estimator,all);
       /* 
-       *  Refine the grid. this always refines 1 time;
+       *  Refine the grid. this always refines 1 time, but since we
+       *  are in a loop, it will refine ref_levels times;
        */
       refine(1,sc,marked); 
       /* free */
@@ -59,11 +77,11 @@ INT main(INT   argc,   char *argv[])
     // refine ref_levels;
     refine(ref_levels,sc,NULL);
   }
-  //  shrink the sc to hold the finest grid only
+  /*  MAKE sc to be the finest grid only */
   scfinalize(sc);
-  // write the output mesh file:    
+  /* write the output mesh file:    */
   hazw(g->fgrid,sc,0);
-  //  fprintf(stdout,"\n\n%%Writing a vtk on file...%s\n",g->fvtu);
+  /* WRITE THE OUTPUT vtu file for paraview:    */
   vtkw(g->fvtu,sc,0,1.);
   /*FREE*/
   input_grid_free(g);
