20e2329bbf87a992829d9b0b45460c86dcf16dc2
Wenfang Xu <hogwarts_jc@hotmail.com>
2019-10-07 01:35:29 -0700

    Add files that were forgotten
    Former-commit-id: 673817bbb5c73a1e6c334313336133b13f93cfa9

--- trabajo/signal_graphs/hamiltonian_algorithm.h
+++ trabajo/signal_graphs/hamiltonian_algorithm.h
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cstdlib>
 #include <numeric>
+#include <queue>
 #include <random>
 #include <vector>
 #include "hazmath_include.h"
@@ -11,7 +12,9 @@ dCSRmat* getRandomProlongation(int n, int N, int seed) {
   std::vector<int> samples(n);
   std::iota(samples.begin(), samples.end(), 0);
   std::shuffle(
-    samples.begin(), samples.end(), std::default_random_engine(seed));
+      samples.begin(),
+      samples.end(),
+      std::default_random_engine(seed));
   std::sort(samples.begin(), samples.begin() + N - n);
 
   dCSRmat *prolong_mat = (dCSRmat*)malloc(sizeof(dCSRmat));
@@ -37,16 +40,84 @@ dCSRmat* getRandomProlongation(int n, int N, int seed) {
   return prolong_mat;
 }
 
-double*
-compress(double* v, const std::vector<int> permutation, int k, int seed) {
+REAL* prolongate(
+    REAL* v,
+    const std::vector<int> permutation,
+    dCSRmat* prolong_mat) {
+  int n = permutation.size();
+
+  REAL permuted_v[n];
+  for (int i = 0; i < n; ++i) {
+    permuted_v[i] = v[permutation[i]];
+  }
+
+  REAL* prolongated_v = (REAL*)calloc(n, sizeof(REAL));
+  dcsr_mxv(prolong_mat, permuted_v, prolongated_v);
+
+  return prolongated_v;
+}
+
+std::vector<REAL*> getWalshBasis(int L) {
+  assert(L >= 0);
+
+  const int N = 1 << L;
+  std::vector<REAL*> basis(N);
+  basis[0] = (REAL*)calloc(N, sizeof(REAL));
+  for (int j = 0; j < N; ++j) {
+    basis[0][j] = 1;
+  }
+
+  std::queue<std::pair<int, int>> q;
+  q.push({1, 0});
+  --L;
+  while (!q.empty()) {
+    int i = q.front().first;
+    int l = q.front().second;
+    const int b = i - (1 << l);
+    basis[i] = (REAL*)calloc(N, sizeof(REAL));
+    for (int j = 0; j < N; ++j) {
+      basis[i][j] = basis[b][j] * ((j >> (L - l)) % 2 ? -1 : 1);
+    }
+
+    if (l < L) {
+      i <<= 1;
+      ++l;
+      q.push({i, l});
+      q.push({i + 1, l});
+    }
+    q.pop();
+  }
+
+  return basis;
+}
+
+template<typename T>
+void deleteArray(const std::vector<T*>& array) {
+  for (auto e : array) {
+    delete e;
+  }
+}
+
+REAL* approximate(REAL* v, int N, int k) {
+
+}
+
+REAL*
+compress(REAL* v, const std::vector<int> permutation, int k, int seed) {
   int n = permutation.size();
   int N = 1;
+  int L = 0;
   while (N < n) {
     N <<= 1;
+    ++L;
   }
   dCSRmat* prolong_mat = getRandomProlongation(n, N, seed);
-  // double* prolongated_v = prolongate(v, permutation, prolong_mat);
-  // double* k_term_approximation = approximate(prolongated_v, N, k);
-  // double* approximation =
+  REAL* prolongated_v = prolongate(v, permutation, prolong_mat);
+  REAL* k_term_approximation = approximate(prolongated_v, N, k);
+  // REAL* approximation =
   //     project(k_term_approximation, prolong_mat, permutation);
+
+  dcsr_free(prolong_mat);
+  delete prolongated_v;
+  // delete k_term_approximation;
 }
--- trabajo/signal_graphs/tests/get_random_prolongation_test.cpp
+++ trabajo/signal_graphs/tests/get_random_prolongation_test.cpp
@@ -1,20 +1,12 @@
+#include "utils.h"
 #include "../hamiltonian_algorithm.h"
 
 int main(int argc, char *argv[]) {
   auto* prolong_mat = getRandomProlongation(3, 5, 0);
-
-  std::vector<int> IA({0, 1, 2, 3, 4, 5});
-  std::vector<int> JA({0, 0, 1, 2, 2});
-  std::vector<int> val({1, 1, 1, 1, 1});
-  for (int i = 0; i < IA.size(); ++i) {
-    assert(prolong_mat->IA[i] == IA[i]);
-  }
-  for (int i = 0; i < JA.size(); ++i) {
-    assert(prolong_mat->JA[i] == JA[i]);
-    assert(prolong_mat->val[i] == val[i]);
-  }
+  assertArraysEqual(prolong_mat->IA, std::vector<int>({0, 1, 2, 3, 4, 5}));
+  assertArraysEqual(prolong_mat->JA, std::vector<int>({0, 0, 1, 2, 2}));
+  assertArraysEqual(prolong_mat->val, std::vector<double>({1, 1, 1, 1, 1}));
 
   dcsr_free(prolong_mat);
-
   return 0;
 }
