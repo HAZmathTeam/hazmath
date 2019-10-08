68248894a326e23bc24fa1f83eb0a2227fa285fd
Wenfang Xu <hogwarts_jc@hotmail.com>
2019-10-07 22:13:46 -0700

    Complete Hamiltonian compression algorithm
    Former-commit-id: e4d4f309de63fc0175f89fff0880f9527a314239

--- trabajo/signal_graphs/hamiltonian_algorithm.h
+++ trabajo/signal_graphs/hamiltonian_algorithm.h
@@ -1,58 +1,44 @@
 #include <algorithm>
+#include <cmath>
 #include <cstdlib>
+#include <functional>
 #include <numeric>
 #include <queue>
 #include <random>
 #include <vector>
 #include "hazmath_include.h"
 
-dCSRmat* getRandomProlongation(int n, int N, int seed) {
-  assert(n < N);
+std::vector<int> getRandomProlongation(int n, int N, int seed) {
+  assert(n <= N);
 
-  std::vector<int> samples(n);
-  std::iota(samples.begin(), samples.end(), 0);
+  std::vector<int> samples(N);
+  std::iota(samples.begin(), samples.begin() + n, 0);
   std::shuffle(
       samples.begin(),
-      samples.end(),
+      samples.begin() + n,
       std::default_random_engine(seed));
-  std::sort(samples.begin(), samples.begin() + N - n);
+  std::iota(samples.begin() + N - n, samples.end(), 0);
+  std::sort(samples.begin(), samples.end());
 
-  dCSRmat *prolong_mat = (dCSRmat*)malloc(sizeof(dCSRmat));
-  *prolong_mat = dcsr_create(N, n, N);
-  auto sample_it = samples.begin();
-  for (int row = 0, vertex = 0; ; ++vertex) {
-    prolong_mat->IA[row] = row;
-    if (row >= N) {
-      break;
-    }
-    prolong_mat->JA[row] = vertex;
-    prolong_mat->val[row] = 1;
-    if (vertex == *sample_it) {
-      ++row;
-      prolong_mat->IA[row] = row;
-      prolong_mat->JA[row] = vertex;
-      prolong_mat->val[row] = 1;
-      ++sample_it;
-    }
-    ++row;
-  }
-
-  return prolong_mat;
+  return samples;
 }
 
-REAL* prolongate(
-    REAL* v,
-    const std::vector<int> permutation,
-    dCSRmat* prolong_mat) {
+std::vector<REAL> prolongate(
+    const std::vector<REAL>& v,
+    const std::vector<int>& permutation,
+    const std::vector<int>& samples) {
   int n = permutation.size();
+  int N = samples.size();
 
-  REAL permuted_v[n];
+  std::vector<REAL> permuted_v(n);
   for (int i = 0; i < n; ++i) {
     permuted_v[i] = v[permutation[i]];
   }
 
-  REAL* prolongated_v = (REAL*)calloc(n, sizeof(REAL));
-  dcsr_mxv(prolong_mat, permuted_v, prolongated_v);
+  std::vector<REAL> prolongated_v(N);
+  for (int i = 0; i < N; ++i) {
+    prolongated_v[i] = permuted_v[samples[i]];
+  }
 
   return prolongated_v;
 }
@@ -62,7 +48,7 @@ std::vector<REAL*> getWalshBasis(int L) {
 
   const int N = 1 << L;
   std::vector<REAL*> basis(N);
-  basis[0] = (REAL*)calloc(N, sizeof(REAL));
+  basis[0] = (REAL*)malloc(N * sizeof(REAL));
   for (int j = 0; j < N; ++j) {
     basis[0][j] = 1;
   }
@@ -74,7 +60,7 @@ std::vector<REAL*> getWalshBasis(int L) {
     int i = q.front().first;
     int l = q.front().second;
     const int b = i - (1 << l);
-    basis[i] = (REAL*)calloc(N, sizeof(REAL));
+    basis[i] = (REAL*)malloc(N * sizeof(REAL));
     for (int j = 0; j < N; ++j) {
       basis[i][j] = basis[b][j] * ((j >> (L - l)) % 2 ? -1 : 1);
     }
@@ -98,12 +84,73 @@ void deleteArray(const std::vector<T*>& array) {
   }
 }
 
-REAL* approximate(REAL* v, int N, int k) {
+std::vector<REAL> approximate(const std::vector<REAL>& v, int L, int k) {
+  int N = 1 << L;
+  auto basis = getWalshBasis(L);
+  std::vector<REAL> inner_products;
+  inner_products.reserve(N);
+  for (auto b : basis) {
+    inner_products.push_back(array_dotprod(N, v.data(), b));
+  }
 
+  std::priority_queue<REAL, std::vector<REAL>, std::greater<REAL>> heap;
+  for (auto product : inner_products) {
+    product = abs(product);
+    if (heap.size() < k) {
+      heap.push(product);
+    } else if (product > heap.top()) {
+      heap.pop();
+      heap.push(product);
+    }
+  }
+  auto kth_largest = heap.top();
+
+  std::vector<REAL> compression(N, 0.0);
+  for (int i = 0; i < N; ++i) {
+    auto product = inner_products[i];
+    if (abs(product) >= kth_largest) {
+      array_axpy(N, product / N, basis[i], compression.data());
+    }
+  }
+
+  deleteArray(basis);
+  return compression;
 }
 
-REAL*
-compress(REAL* v, const std::vector<int> permutation, int k, int seed) {
+std::vector<REAL> project(
+    const std::vector<REAL>& k_term_approximation,
+    const std::vector<int>& samples,
+    const std::vector<int>& permutation) {
+  int n = permutation.size();
+
+  std::vector<REAL> projection;
+  projection.reserve(n);
+  projection.push_back({k_term_approximation[0]});
+  int curr_count = 1;
+  for (int i = 1; i < k_term_approximation.size(); ++i) {
+    if (samples[i] != samples[i - 1]) {
+      projection.back() /= curr_count;
+      projection.push_back(k_term_approximation[i]);
+      curr_count = 1;
+    } else {
+      projection.back() += k_term_approximation[i];
+      ++curr_count;
+    }
+  }
+
+  std::vector<REAL> approximation(n);
+  for (int i = 0; i < n; ++i) {
+    approximation[permutation[i]] = projection[i];
+  }
+
+  return approximation;
+}
+
+std::vector<REAL> compress(
+    const std::vector<REAL>& v,
+    const std::vector<int> permutation,
+    int k,
+    int seed) {
   int n = permutation.size();
   int N = 1;
   int L = 0;
@@ -111,13 +158,12 @@ compress(REAL* v, const std::vector<int> permutation, int k, int seed) {
     N <<= 1;
     ++L;
   }
-  dCSRmat* prolong_mat = getRandomProlongation(n, N, seed);
-  REAL* prolongated_v = prolongate(v, permutation, prolong_mat);
-  REAL* k_term_approximation = approximate(prolongated_v, N, k);
-  // REAL* approximation =
-  //     project(k_term_approximation, prolong_mat, permutation);
 
-  dcsr_free(prolong_mat);
-  delete prolongated_v;
-  // delete k_term_approximation;
+  const auto samples = getRandomProlongation(n, N, seed);
+  const std::vector<REAL> prolongated_v = prolongate(v, permutation, samples);
+  const std::vector<REAL> k_term_approximation = approximate(prolongated_v, L, k);
+  std::vector<REAL> approximation =
+      project(k_term_approximation, samples, permutation);
+
+  return approximation;
 }
