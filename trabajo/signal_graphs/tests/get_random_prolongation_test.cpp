74e6812ab06d0947a8865fbe306ed450caffa663
Wenfang Xu <hogwarts_jc@hotmail.com>
2019-10-07 22:13:46 -0700

    Complete Hamiltonian compression algorithm
    Former-commit-id: 68248894a326e23bc24fa1f83eb0a2227fa285fd [formerly e4d4f309de63fc0175f89fff0880f9527a314239]
    Former-commit-id: f85be439f30710b0fb74761f4960fb9156fd93ca

--- trabajo/signal_graphs/tests/get_random_prolongation_test.cpp
+++ trabajo/signal_graphs/tests/get_random_prolongation_test.cpp
@@ -2,11 +2,8 @@
 #include "../hamiltonian_algorithm.h"
 
 int main(int argc, char *argv[]) {
-  auto* prolong_mat = getRandomProlongation(3, 5, 0);
-  assertArraysEqual(prolong_mat->IA, std::vector<int>({0, 1, 2, 3, 4, 5}));
-  assertArraysEqual(prolong_mat->JA, std::vector<int>({0, 0, 1, 2, 2}));
-  assertArraysEqual(prolong_mat->val, std::vector<double>({1, 1, 1, 1, 1}));
+  auto samples = getRandomProlongation(3, 5, 0);
+  assertArraysEqual(samples.data(), std::vector<int>({0, 0, 1, 2, 2}));
 
-  dcsr_free(prolong_mat);
   return 0;
 }
