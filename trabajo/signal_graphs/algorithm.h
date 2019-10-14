0e750242dbaec45da8305a2668c1e965d7517a5f
Wenfang Xu <hogwarts_jc@hotmail.com>
2019-10-13 23:46:10 -0700

    Apply clang-format
    Former-commit-id: 7508d08a41fcf8c49bd9268333b82fd8d0655d9f

--- trabajo/signal_graphs/algorithm.h
+++ trabajo/signal_graphs/algorithm.h
@@ -5,12 +5,14 @@
 #include "hazmath_include.h"
 
 void setupHierarchy(const char *file, dCSRmat *&A,
-    std::vector<dCSRmat *> &Qj_array, std::vector<int> &Nj_array);
+                    std::vector<dCSRmat *> &Qj_array,
+                    std::vector<int> &Nj_array);
 
-void compAndDecomp(double *v, dCSRmat *A, const std::vector<dCSRmat *> &Qj_array,
-    const std::vector<int> &Nj_array, int threshold, double p, double* v2,
-    double *v3);
+void compAndDecomp(double *v, dCSRmat *A,
+                   const std::vector<dCSRmat *> &Qj_array,
+                   const std::vector<int> &Nj_array, int threshold, double p,
+                   double *v2, double *v3);
 
-std::vector<int> getHamiltonianPath(Tree* tree);
+std::vector<int> getHamiltonianPath(Tree *tree);
 
 #endif
--- trabajo/signal_graphs/ex2.cpp
+++ trabajo/signal_graphs/ex2.cpp
@@ -4,14 +4,14 @@
  *   ./ex2 graphs/Precip/AgMERRA_Precip_by_mo_tot/agp1980_ -k 100
  *   ./ex2 graphs/Precip/AgMERRA_Precip_by_mo_tot/agp1980_ -l
  */
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <iterator>
-#include <cassert>
-#include <unistd.h>
-#include "graph.h"
 #include "algorithm.h"
+#include "graph.h"
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <unistd.h>
 
 using namespace std;
 
@@ -24,26 +24,24 @@ int main(int argc, char *argv[]) {
 
   while ((c = getopt(argc, argv, "k:l")) != -1) {
     switch (c) {
-      case 'k':
-        threshold = stoi(optarg);
-        opt_k = true;
-        break;
-      case 'l':
-        opt_l = true;
-        break;
-      case '?':
-        if (optopt == 'k') {
-          fprintf(stderr, "Option -%c requires an argument.\n", optopt);
-        }
-        else if (isprint(optopt)) {
-          fprintf(stderr, "Unknown option `-%c'.\n", optopt);
-        }
-        else {
-          fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
-        }
-        return 1;
-      default:
-        abort();
+    case 'k':
+      threshold = stoi(optarg);
+      opt_k = true;
+      break;
+    case 'l':
+      opt_l = true;
+      break;
+    case '?':
+      if (optopt == 'k') {
+        fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+      } else if (isprint(optopt)) {
+        fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+      } else {
+        fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
+      }
+      return 1;
+    default:
+      abort();
     }
   }
   if (optind != argc - 1) {
@@ -55,15 +53,15 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  int side = 512, nnz = (side-1)*side*2, edge_count = 0, n = side*side;
+  int side = 512, nnz = (side - 1) * side * 2, edge_count = 0, n = side * side;
   if (threshold > n - 1) {
     threshold = n - 1;
   }
   ofstream tempfile("temp");
-  tempfile << side*side << ' ' << side*side << ' ' << nnz << '\n';
+  tempfile << side * side << ' ' << side * side << ' ' << nnz << '\n';
   for (int i = 0; i < side; ++i) {
     for (int j = 0; j < side; ++j) {
-      int index = i*side + j + 1;
+      int index = i * side + j + 1;
       if (j < side - 1) {
         tempfile << index << ' ' << index + 1 << '\n';
         ++edge_count;
@@ -86,23 +84,24 @@ int main(int argc, char *argv[]) {
   // Compress/decompress a smooth vector and compute the error
   const string prefix(argv[optind]);
 
-  vector<string> months{"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"};
+  vector<string> months{"01", "02", "03", "04", "05", "06",
+                        "07", "08", "09", "10", "11", "12"};
   for (auto mon : months) {
-    string filename = prefix+mon;
+    string filename = prefix + mon;
     ifstream file(filename.c_str());
     string line;
     vector<vector<double>> matrix;
     while (getline(file, line)) {
       istringstream iss(line);
-      matrix.push_back(vector<double>(
-          istream_iterator<double>(iss), istream_iterator<double>()));
+      matrix.push_back(vector<double>(istream_iterator<double>(iss),
+                                      istream_iterator<double>()));
     }
     file.close();
     for (auto row : matrix) {
       assert(matrix.size() == row.size());
     }
 
-    REAL *v = (REAL *)malloc(sizeof(REAL)*n);
+    REAL *v = (REAL *)malloc(sizeof(REAL) * n);
     int v_ind = 0;
     for (auto row : matrix) {
       for (auto entry : row) {
@@ -111,27 +110,27 @@ int main(int argc, char *argv[]) {
     }
 
     cout << endl << endl << "Month" + mon << endl;
-    REAL *v2 = (REAL *)malloc(sizeof(REAL)*n);
-    REAL *v3 = (REAL *)malloc(sizeof(REAL)*n);
+    REAL *v2 = (REAL *)malloc(sizeof(REAL) * n);
+    REAL *v3 = (REAL *)malloc(sizeof(REAL) * n);
     if (!opt_l) {
       compAndDecomp(v, A, Qj_array, Nj_array, threshold, 1.0, v2, v3);
 
-      auto write = [&] (string filename, REAL data[]) {
+      auto write = [&](string filename, REAL data[]) {
         ofstream ofs(filename);
         for (int i = 0; i < side; ++i) {
-          for (int j = 0; ; ++j) {
-            ofs << data[i*side+j];
-            if (j == side - 1) break;
+          for (int j = 0;; ++j) {
+            ofs << data[i * side + j];
+            if (j == side - 1)
+              break;
             ofs << ' ';
           }
           ofs << '\n';
         }
         ofs.close();
       };
-      write(filename+"_1", v2);
-      write(filename+"_2", v3);
-    }
-    else {
+      write(filename + "_1", v2);
+      write(filename + "_2", v3);
+    } else {
       string ofilename = filename;
       ofilename.insert(ofilename.rfind('/'), "/levels");
       ofstream ofs(ofilename + ".data");
