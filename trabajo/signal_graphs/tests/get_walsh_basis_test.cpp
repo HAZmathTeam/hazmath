aa8fc553411fc02ebe039810f3a0efe2d6203814
Wenfang Xu <hogwarts_jc@hotmail.com>
2019-10-13 23:46:10 -0700

    Apply clang-format
    Former-commit-id: 4dee5e9555a93ce3962cde04365b0b5e16010250 [formerly 7508d08a41fcf8c49bd9268333b82fd8d0655d9f]
    Former-commit-id: 62d83eb3bb2331bc261066a0ca8994a70640c27b

--- trabajo/signal_graphs/tests/get_walsh_basis_test.cpp
+++ trabajo/signal_graphs/tests/get_walsh_basis_test.cpp
@@ -1,5 +1,5 @@
-#include "utils.h"
 #include "../hamiltonian_algorithm.h"
+#include "utils.h"
 
 int main() {
   const std::vector<std::vector<std::vector<double>>> expected_bases(
@@ -16,7 +16,7 @@ int main() {
         {1, -1, -1, 1, -1, 1, 1, -1}}});
   for (int l = 0; l < expected_bases.size(); ++l) {
     auto basis = getWalshBasis(l);
-    auto& expected_basis = expected_bases[l];
+    auto &expected_basis = expected_bases[l];
     for (int i = 0; i < expected_basis.size(); ++i) {
       assertArraysEqual(basis[i], expected_basis[i]);
     }
