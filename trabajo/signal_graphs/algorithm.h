057238b73fa7ca8887b93fe70ffb88839ad6cd0a
Wenfang Xu <hogwarts_jc@hotmail.com>
2019-11-22 01:24:53 -0800

    [Signals graphs] Convert algorithms into classes
    Former-commit-id: 8f2418e970fdbee6bf59c25fdf6c49d945ba48e5

--- trabajo/signal_graphs/algorithm.h
+++ trabajo/signal_graphs/algorithm.h
@@ -6,32 +6,38 @@
 
 class Algorithm {
 public:
-  virtual int numBlocks(int numBlocks) const = 0;
+  virtual bool isAdaptive() const { return false; }
+
+  void setupHierarchy(Graph graph, std::vector<dCSRmat *> &Qj_array,
+                      std::vector<int> &Nj_array) const;
+
+  void compAndDecomp(int n, double *v, const std::vector<dCSRmat *> &Qj_array,
+                     int largestK, double p, double *v2) const;
+
+  void compAndDecomp(int n, double *v, const std::vector<dCSRmat *> &Qj_array,
+                     const std::vector<int> &Nj_array, int largestK, double p,
+                     double *v3) const;
+
+  void compAndDecomp(const Graph &graph, REAL *v, const int largestK,
+                     const double p) const;
+
+private:
+  virtual int getNumBlocks(int numBlocks) const = 0;
 };
 
 class Adaptive : public Algorithm {
 public:
-  int numBlocks(int numBlocks) const { return numBlocks; }
+  bool isAdaptive() const { return true; }
+
+private:
+  int getNumBlocks(int numBlocks) const { return numBlocks; }
 };
 
 class Gtbwt : public Algorithm {
-public:
-  int numBlocks(int numBlocks) const { return 1; }
+private:
+  int getNumBlocks(int numBlocks) const { return 1; }
 };
 
-void setupHierarchy(Graph graph, std::vector<dCSRmat *> &Qj_array,
-                    std::vector<int> &Nj_array, const Algorithm &algorithm);
-
-void compAndDecomp(int n, double *v, const std::vector<dCSRmat *> &Qj_array,
-                   int largestK, double p, double *v2);
-
-void compAndDecomp(int n, double *v, const std::vector<dCSRmat *> &Qj_array,
-                   const std::vector<int> &Nj_array, int largestK, double p,
-                   double *v3);
-
 std::vector<int> getHamiltonianPath(Tree *tree);
-
-void compAndDecomp(const Graph &graph, REAL *v, const int largestK,
-                   const double p, const Algorithm &algorithm);
 
 #endif
