95b1ba37f78c38fe88a7187636f16c5ef879e15d
Ludmil T Zikatanov <ludmil02@gmail.com>
2020-03-08 13:41:30 -0400

    almost done with disk meshes
    Former-commit-id: 3a7d650664e14f163dc57cb9c6f7c1f67250ed92

--- trabajo/gridding/disk_meshes/m_hazw.m
+++ trabajo/gridding/disk_meshes/m_hazw.m
@@ -1,39 +1,28 @@
-  function stat0=triang_W(t,p,fname0);
-%%%%  Writes a triangulation t,p constructed with distmesh; 
-n = length(p(1,:));
-nel = length(t(1,:))
+function stat0=m_hazw(t,p,fname0);
+nssss=size(p)
+
+%%%%  Writes a triangulation t,p constructed with distmesh;
+nv = size(p,1)
+nt = size(t,1)
+dim1=size(t,2);
+dim=dim1-1
 fid = fopen(fname0,'w');
 if(fid > 0)
      disp(['Writing the mesh to: ',fname0]);
      %% write number of elements, number of nodes and twice number
      %of boundary edges
-fprintf(fid,' %i %i %i %i\n',nel,n,ned,ned);
-%%% Write T row by row;
-for j = 1 : 4
- fprintf(fid,' %i ',t(j,1:nel));
- fprintf(fid,'\n');
-end
-%%% Write coordinates of the nodes;
-fprintf(fid,' %23.17e ',p(1,1:n));
-fprintf(fid,'\n');
-fprintf(fid,' %23.17e ',p(2,1:n));
-fprintf(fid,'\n');
-for j = 1 : 2
-%%% When j=1: Write the node numbers of the first end point of the boundary edges;
-%%% When j=2: Write the node numbers of the SECOND end point of the boundary edges;
- fprintf(fid,' %i ',e(j,1:ned));
- fprintf(fid,'\n');
-end
-%% ignore these. 
-for j = 3 : 4
-fprintf(fid,' %23.17e ',e(j,1:ned));
- fprintf(fid,'\n');
-end
-for j = 5 : 7
- fprintf(fid,' %i ',e(j,1:ned));
- fprintf(fid,'\n');
-end
-stat0=fclose(fid);
+     fprintf(fid,' %i %i %i %i\n',nt,nv,dim,0);
+     %%% Write T row by row;
+     for j = 1 : dim1
+     fprintf(fid,' %i ',transpose(t(1:nt,j)));
+     fprintf(fid,'\n');
+     end
+     %%% Write coordinates of the nodes;
+     for j = 1 : dim
+     fprintf(fid,' %23.17g ',transpose(p(1:nv,j)));
+     fprintf(fid,'\n');
+     end
+     stat0=fclose(fid);
 else
-disp(['error opening file **',fnam0,' **'])
+  disp(['error opening file **',fnam0,' **'])
 end
