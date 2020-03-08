c35698365a5697801c618653cfd7a815efcd8dd8
Ludmil T Zikatanov <ludmil02@gmail.com>
2020-03-08 17:42:40 -0600

    done generating circle meshes
    Former-commit-id: 81a1c6d22b87ed2cb9fbf56d7dff39df07794a89 [formerly 992ba6858d0df533e4a81ba6e721b190feca175e]
    Former-commit-id: 7b9f74da19a0218660685bbf8802618697b20ed5

--- trabajo/gridding/disk_meshes/run_disk_mesh.m
+++ trabajo/gridding/disk_meshes/run_disk_mesh.m
@@ -1,7 +1,10 @@
 %% R is the radius of the circle. the mesh size is about
 %% h=R*2^(-level-1)
-R=1; level=2;
+R=1; level=0;
 [t,xy]=disk_mesh(R,level);
-stat0=m_hazw(t,xy,'zzz.haz');
-
-%%[VT,EV,ET,TT] = bndrypts(t);
+bcodes = bndrypts(t);
+%% pick a filename
+fname0=['circle_level',int2str(level),'.haz'];
+stat0=m_hazw(fname0,t,xy,bcodes)
+%%% just to check whether we really found the boundary points
+bp=find(bcodes); hold on ; plot(xy(bp,1),xy(bp,2),'ro',xy(bp,1),xy(bp,2),'r*'); hold off;
