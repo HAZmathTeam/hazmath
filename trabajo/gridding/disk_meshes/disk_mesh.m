ed0011b7b70e6e34e330005349ab0e4023e7b4ce
Ludmil T Zikatanov <ludmil02@gmail.com>
2020-03-08 06:57:18 -0400

    working on  meshes on a disk made with distmesh
    Former-commit-id: 111644b855cb5ef97a839603bc2e833b3e4f73b0 [formerly b205caed20339a740b3c75c7e00f2f890203d98b]
    Former-commit-id: f6b5195862e74d74318a9c8c9b1aa17aa655b8ef

---
+++ trabajo/gridding/disk_meshes/disk_mesh.m
@@ -0,0 +1,12 @@
+function [t,x,y]=disk_mesh(R,level)
+%% using distmesh: the mesh size is R*2^(-level-1)
+warning('off', 'Octave:possible-matlab-short-circuit-operator');
+warning('off','all');
+addpath("distmesh/");
+fd=@(p) sqrt(sum(p.^2,2))-R;
+h=R*2^(-(level+1));
+disp(['Disk domain: R=',num2str(R),'; step size h=',num2str(h)]);
+[xy,t]=distmesh2d(fd,@huniform,h,[-R,-R;R,R],[]);
+x=xy(:,1);y=xy(:,2);
+disp(['Elements=',int2str(size(t,1)),'; Verices=',int2str(size(x,1))]);
+%% return
