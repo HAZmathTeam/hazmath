c226edd8169a5e57ddeda672be0c3757e6d1fc9b
Ludmil T Zikatanov <ludmil02@gmail.com>
2020-03-08 13:41:30 -0400

    almost done with disk meshes
    Former-commit-id: 83cbfa0956a4df22510e17d7403d932071d9b187 [formerly 3a7d650664e14f163dc57cb9c6f7c1f67250ed92]
    Former-commit-id: 4d130ceff3003bcc5b6398750031bac8f0957938

--- trabajo/gridding/disk_meshes/disk_mesh.m
+++ trabajo/gridding/disk_meshes/disk_mesh.m
@@ -1,4 +1,4 @@
-function [t,x,y]=disk_mesh(R,level)
+function [t,xy]=disk_mesh(R,level)
 %% using distmesh: the mesh size is R*2^(-level-1)
 warning('off', 'Octave:possible-matlab-short-circuit-operator');
 warning('off','all');
@@ -7,6 +7,6 @@ fd=@(p) sqrt(sum(p.^2,2))-R;
 h=R*2^(-(level+1));
 disp(['Disk domain: R=',num2str(R),'; step size h=',num2str(h)]);
 [xy,t]=distmesh2d(fd,@huniform,h,[-R,-R;R,R],[]);
-x=xy(:,1);y=xy(:,2);
-disp(['Elements=',int2str(size(t,1)),'; Verices=',int2str(size(x,1))]);
-%% return
+size(xy)
+disp(['Elements=',int2str(size(t,1)),'; Verices=',int2str(size(xy,1))]);
+return
--- trabajo/gridding/disk_meshes/run_disk_mesh.m
+++ trabajo/gridding/disk_meshes/run_disk_mesh.m
@@ -1,8 +1,7 @@
 %% R is the radius of the circle. the mesh size is about
 %% h=R*2^(-level-1)
 R=1; level=2;
-[t,x,y]=disk_mesh(R,level);
+[t,xy]=disk_mesh(R,level);
+stat0=m_hazw(t,xy,'zzz.haz');
 
 %%[VT,EV,ET,TT] = bndrypts(t);
-
-
