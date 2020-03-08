85381838e1e02d267899daab0fa8f80e083c1bbb
Ludmil T Zikatanov <ludmil02@gmail.com>
2020-03-08 17:42:40 -0600

    done generating circle meshes
    Former-commit-id: fc6071d8f176f29c527e7f9858c73a6a68dd1e45 [formerly 992ba6858d0df533e4a81ba6e721b190feca175e]
    Former-commit-id: 7b9f74da19a0218660685bbf8802618697b20ed5

--- trabajo/gridding/disk_meshes/bndrypts.m
+++ trabajo/gridding/disk_meshes/bndrypts.m
@@ -1,12 +1,19 @@
-function [VT,EV,ET,TT] = bndrypts(t);
+function bcodes = bndrypts(t,c);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%% function bcodes = bndrypts(t,c);
+%% finds the boundary nodes and sets their code to "c"
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 t=transpose(t); %% this is fake just to get the incidence matrices.
-[idummy,nel]=size(t);
+[dim1,nel]=size(t);
+dim=dim1-1;
 iT=[[1:nel];[1:nel];[1:nel]];
-iT=reshape(iT,3*nel,1);
-t=reshape(t(1:3,1:nel),3*nel,1);
-%% incidence vertex -- triangle 
-VT=sparse(t,iT,ones(3*nel,1));
-t=reshape(t,3,nel);
+iT=reshape(iT,dim1*nel,1);
+t=reshape(t(1:dim1,1:nel),dim1*nel,1);
+%% incidence vertex -- triangle
+VT=sparse(t,iT,ones(dim1*nel,1));
+t=reshape(t,dim1,nel);
 t=transpose(t);
 [nv,nt]=size(VT);
 %% incidence: edge--vertex
@@ -16,12 +23,17 @@ ne=length(iw);
 iev=[[1:ne]';[1:ne]'];jev=[iw;jw];
 EV=sparse(iev,jev,ones(2*ne,1),ne,nv);
 %% incidence: edge--triangle
-[iw,jw]=find(EV*VT == 2);
+[iw,jw]=find(EV*VT == dim);
 ET=sparse(iw,jw,ones(length(iw),1),ne,nt);
 
-%% we need to find the T,T matrix:
-TT = transpose(VT)*VT;
-[i,j,s] = find(TT==2);
-TT=sparse(i,j,s,nt,nt);
+ou=find((ET*ones(nt,1))<2);
+[~,bp]=find(EV(ou,1:nv));
+bcodes=zeros(nv,1);
+bcodes(bp)=ones(length(bp),1);
 
+%% no need to find the T,T matrix:
+%TT = transpose(VT)*VT;
+%[i,j,s] = find(TT==dim);
+%TT=sparse(i,j,s,nt,nt);
 
+return
