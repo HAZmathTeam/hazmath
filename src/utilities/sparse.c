3b711581335fabaa27b6c0b50afc295d77022815
Ludmil T Zikatanov <ludmil.math@gmail.com>
2020-05-07 20:57:11 -0400

    profile again
    Former-commit-id: 1ec6b0a747444de81d8f0c17699910a79195a6e4 [formerly 0c1f8e2fcd60c268f798079fd454717390bd9d7c]
    Former-commit-id: 61b0bef142a9d26553cab508a1ccbc7d8218198e

--- src/utilities/sparse.c
+++ src/utilities/sparse.c
@@ -1743,106 +1743,6 @@ void dcsr_mxm(dCSRmat *A,
 
   C->nnz = C->IA[C->row]-C->IA[0];
 }
-
-/***********************************************************************************************/
-/*!
-   * \fn void icsr_mxm (iCSRmat *A, iCSRmat *B, iCSRmat *C)
-   *
-   * \brief Sparse matrix multiplication C=A*B (index starts with 0!!)
-   *
-   * \param A   Pointer to the iCSRmat matrix A
-   * \param B   Pointer to the iCSRmat matrix B
-   * \param C   Pointer to iCSRmat matrix equal to A*B
-   *
-   */
-void icsr_mxm(iCSRmat *A,
-              iCSRmat *B,
-              iCSRmat *C)
-{
-  INT i,j,k,l,count;
-
-  INT *JD = (INT *)calloc(B->col,sizeof(INT));
-
-  C->row = A->row;
-  C->col = B->col;
-  C->val = NULL;
-  C->JA  = NULL;
-  C->IA  = (INT*)calloc(C->row+1,sizeof(INT));
-
-  for (i=0;i<B->col;++i) JD[i]=-1;
-
-  // step 1: Find first the structure IA of C
-  for(i=0;i<C->row;++i) {
-    count=0;
-
-    for (k=A->IA[i];k<A->IA[i+1];++k) {
-      for (j=B->IA[A->JA[k]];j<B->IA[A->JA[k]+1];++j) {
-        for (l=0;l<count;l++) {
-          if (JD[l]==B->JA[j]) break;
-        }
-
-        if (l==count) {
-          JD[count]=B->JA[j];
-          count++;
-        }
-      }
-    }
-    C->IA[i+1]=count;
-    for (j=0;j<count;++j) {
-      JD[j]=-1;
-    }
-  }
-
-  for (i=0;i<C->row;++i) C->IA[i+1]+=C->IA[i];
-
-  // step 2: Find the structure JA of C
-  INT countJD;
-
-  C->JA=(INT*)calloc(C->IA[C->row],sizeof(INT));
-
-  for (i=0;i<C->row;++i) {
-    countJD=0;
-    count=C->IA[i];
-    for (k=A->IA[i];k<A->IA[i+1];++k) {
-      for (j=B->IA[A->JA[k]];j<B->IA[A->JA[k]+1];++j) {
-        for (l=0;l<countJD;l++) {
-          if (JD[l]==B->JA[j]) break;
-        }
-
-        if (l==countJD) {
-          C->JA[count]=B->JA[j];
-          JD[countJD]=B->JA[j];
-          count++;
-          countJD++;
-        }
-      }
-    }
-
-    //for (j=0;j<countJD;++j) JD[j]=-1;
-    memset(JD, -1, sizeof(INT)*countJD);
-  }
-
-  free(JD);
-
-  // step 3: Find the structure A of C
-  C->val=(INT*)calloc(C->IA[C->row],sizeof(INT));
-
-  for (i=0;i<C->row;++i) {
-    for (j=C->IA[i];j<C->IA[i+1];++j) {
-      C->val[j]=0;
-      for (k=A->IA[i];k<A->IA[i+1];++k) {
-        for (l=B->IA[A->JA[k]];l<B->IA[A->JA[k]+1];l++) {
-          if (B->JA[l]==C->JA[j]) {
-            C->val[j]+=A->val[k]*B->val[l];
-          } // end if
-        } // end for l
-      } // end for k
-    } // end for j
-  }    // end for i
-
-  C->nnz = C->IA[C->row]-C->IA[0];
-}
-
 /***********************************************************************************************/
 /*!
    * \fn void icsr_mxm_symb (iCSRmat *A, iCSRmat *B, iCSRmat *C)
@@ -1855,219 +1755,14 @@ void icsr_mxm(iCSRmat *A,
    *
    */
 void icsr_mxm_symb(iCSRmat *A,
-                   iCSRmat *B,
-                   iCSRmat *C)
+		   iCSRmat *B,
+		   iCSRmat *C)
 {
-  INT i,j,k,l,count;
-
-  INT *JD = (INT *)calloc(B->col,sizeof(INT));
-
-  C->row = A->row;
-  C->col = B->col;
-  C->val = NULL;
-  C->JA  = NULL;
-  C->IA  = (INT*)calloc(C->row+1,sizeof(INT));
-
-  for (i=0;i<B->col;++i) JD[i]=-1;
-
-  // step 1: Find first the structure IA of C
-  for(i=0;i<C->row;++i) {
-    count=0;
-
-    for (k=A->IA[i];k<A->IA[i+1];++k) {
-      for (j=B->IA[A->JA[k]];j<B->IA[A->JA[k]+1];++j) {
-        for (l=0;l<count;l++) {
-          if (JD[l]==B->JA[j]) break;
-        }
-
-        if (l==count) {
-          JD[count]=B->JA[j];
-          count++;
-        }
-      }
-    }
-    C->IA[i+1]=count;
-    for (j=0;j<count;++j) {
-      JD[j]=-1;
-    }
-  }
-
-  for (i=0;i<C->row;++i) C->IA[i+1]+=C->IA[i];
-
-  // step 2: Find the structure JA of C
-  INT countJD;
-
-  C->JA=(INT*)calloc(C->IA[C->row],sizeof(INT));
-
-  for (i=0;i<C->row;++i) {
-    countJD=0;
-    count=C->IA[i];
-    for (k=A->IA[i];k<A->IA[i+1];++k) {
-      for (j=B->IA[A->JA[k]];j<B->IA[A->JA[k]+1];++j) {
-        for (l=0;l<countJD;l++) {
-          if (JD[l]==B->JA[j]) break;
-        }
-
-        if (l==countJD) {
-          C->JA[count]=B->JA[j];
-          JD[countJD]=B->JA[j];
-          count++;
-          countJD++;
-        }
-      }
-    }
-
-    //for (j=0;j<countJD;++j) JD[j]=-1;
-    memset(JD, -1, sizeof(INT)*countJD);
-  }
-
-  free(JD);
-
-  C->nnz = C->IA[C->row]-C->IA[0];
-}
-
-/***********************************************************************************************/
-/*!
-   * \fn void icsr_mxm_symb_max (iCSRmat *A, iCSRmat *B, iCSRmat *C, INT multmax)
-   *
-   * \brief symbolic sparse matrix multiplication C=A*B (index starts with 0!!)
-   *
-   * \param A         Pointer to the iCSRmat matrix A
-   * \param B         Pointer to the iCSRmat matrix B
-   * \param C         Pointer to iCSRmat matrix equal to A*B
-   * \param multimax  value allowed in the iCSRmat matrix C, any entry that is not equal to multimax will be deleted
-   *
-   */
-void zicsr_mxm_symb_max(iCSRmat *A,
-                       iCSRmat *B,
-                       iCSRmat *C,
-                       INT multmax)
-{
-  INT i,j,k,l,count;
-
-  INT *JD = (INT *)calloc(B->col,sizeof(INT));
-  INT *entry_count = (INT *)calloc(B->col,sizeof(INT));
-
-  C->row = A->row;
-  C->col = B->col;
-  C->val = NULL;
-  C->JA  = NULL;
-  C->IA  = (INT*)calloc(C->row+1,sizeof(INT));
-
-  for (i=0;i<B->col;++i) {
-    JD[i] = -1;
-    entry_count[i] = 0;
-  }
-
-  // step 1: Find first the structure IA of C
-  for(i=0;i<C->row;++i) {
-    count=0;
-
-    for (k=A->IA[i];k<A->IA[i+1];++k) {
-      for (j=B->IA[A->JA[k]];j<B->IA[A->JA[k]+1];++j) {
-        for (l=0;l<count;l++) {
-          if (JD[l]==B->JA[j]) {
-            entry_count[l] = entry_count[l]+1;
-            break;
-          }
-        }
-
-        if (l==count) {
-          JD[count]=B->JA[j];
-          entry_count[count] = 1;
-          count++;
-        }
-      }
-
-
-    }
-
-
-    C->IA[i+1]=count;
-
-    for (j=0;j<count;++j) {
-
-      JD[j]=-1;
-
-      if (entry_count[j] != multmax) C->IA[i+1] = C->IA[i+1]-1;
-
-      entry_count[j] = 0;
-
-    }
-
-
-  }
-
-  for (i=0;i<C->row;++i) C->IA[i+1]+=C->IA[i];
-
-
-  // step 2: Find the structure JA of C
-  INT countJD;
-
-  C->JA=(INT*)calloc(C->IA[C->row],sizeof(INT));
-
-  for (i=0;i<C->row;++i) {
-    countJD=0;
-    count=C->IA[i];
-
-    for (k=A->IA[i];k<A->IA[i+1];++k) {
-      for (j=B->IA[A->JA[k]];j<B->IA[A->JA[k]+1];++j) {
-        for (l=0;l<countJD;l++) {
-          if (JD[l]==B->JA[j]) {
-            entry_count[l] = entry_count[l]+1;
-            break;
-          }
-        }
-
-        if (l==countJD) {
-          //C->JA[count]=B->JA[j];
-          JD[countJD]=B->JA[j];
-          entry_count[countJD] = 1;
-          //count++;
-          countJD++;
-        }
-      }
-
-    }
-
-    for (j=0;j<countJD;++j) {
-      if (entry_count[j] == multmax) {
-        C->JA[count]=JD[j];
-        count++;
-      }
-      JD[j]=-1;
-      entry_count[j] = 0;
-    }
-  }
-
-  // free
-  free(JD);
-  free(entry_count);
-
-  C->nnz = C->IA[C->row]-C->IA[0];
-}
-
-void icsr_mxm_symb_max(iCSRmat *A,
-                       iCSRmat *B,
-                       iCSRmat *C,
-                       INT multmax)
-{
-  /* C===================================================================== */
-  /*       subroutine abybs(ia,ja,ib,jb,np,nq,nr,ic,jc,ix) */
-  /* C===================================================================== */
-  /*       implicit real*8(a-h,o-z) */
-  /*       dimension ia(1),ja(1),ib(1),jb(1),ic(1),jc(1),ix(nr) */
-  /* C-------------------------------------------------------------------- */
-  /* C...  SYMBOLIC MULTIPLICATION OF TWO GENERAL SPARSE MATRICIES */
-  /* C...    np = number of rows of a */
-  /* C...    nq = number of columns of a */
-  /* C...    nr = number of columns of b */
-  /* C-------------------------------------------------------------------- */
   INT np=A->row,nr=B->col; // INT nq=A->col;  
   INT i,j,k,nnz,jp,kp;
   C->val=NULL;C->JA=NULL;
   INT *ia=A->IA, *ja=A->JA,*ib=B->IA, *jb=B->JA;
-  INT *ic, *jc, *c;
+  INT *ic, *jc;
   INT *ix=(INT *)calloc(nr,sizeof(INT));
   nnz = 0;
   for(i=0;i<nr;i++)ix[i]=-1;
@@ -2102,8 +1797,42 @@ void icsr_mxm_symb_max(iCSRmat *A,
     }
   }
   ic[np] = nnz;
-  c=(INT *)calloc(nnz,sizeof(INT));  
-/* C===================================================================== */
+  free(ix);
+  C->nnz=nnz;
+  C->row=np; C->col=nr;
+  /**/
+  C->IA=ic;
+  C->JA=jc;
+  C->val=NULL;
+  return;
+}
+/**************************************************************************/
+/*!
+   * \fn void icsr_mxm_symb_max (iCSRmat *A, iCSRmat *B, iCSRmat *C, INT multmax)
+   *
+   * \brief symbolic sparse matrix multiplication C=A*B (index starts with 0!!)
+   *
+   * \param A         Pointer to the iCSRmat matrix A
+   * \param B         Pointer to the iCSRmat matrix B
+   * \param C         Pointer to iCSRmat matrix equal to A*B
+   *
+   */
+void icsr_mxm (iCSRmat *A,iCSRmat *B,iCSRmat *C)
+{
+  /* C-------------------------------------------------------------------- */
+  /* call the symbolic muultiplication */
+  icsr_mxm_symb(A,B,C);
+  /**/  
+  INT np=C->row,nr=C->col,nnz=C->nnz; // INT nq=A->col;  
+  INT i,j,k,jp,kp,aij;
+  INT *ia=A->IA, *ja=A->JA,*ib=B->IA, *jb=B->JA;
+  INT *ic=C->IA, *jc=C->JA;
+  INT *a=A->val, *b=B->val;
+  /**/
+  INT *c=(INT *)calloc(nnz,sizeof(INT));
+  INT *ix=(INT *)calloc(nr,sizeof(INT));
+  /*do the multipplication*/
+  /* C===================================================================== */
 /*       subroutine abyb(ia,ja,ib,jb,np,ic,jc,an,bn,cn,x,nq) */
 /* C===================================================================== */
 /*       implicit real*8 (a-h,o-z) */
@@ -2113,6 +1842,51 @@ void icsr_mxm_symb_max(iCSRmat *A,
 /* C...  MULTIPLICATION OF TWO GENERAL SPARSE MATRICIES: C = A*B */
 /* C-------------------------------------------------------------------- */
 //  ix=realloc(ix,nr*sizeof(INT));
+  for(i=0;i<np;i++){
+    for(j = ic[i];j<ic[i+1];j++) ix[jc[j]] = 0;    
+    for(jp=ia[i];jp<ia[i+1];jp++){
+      j = ja[jp];
+      aij = a[jp]; 
+      for(kp=ib[j];kp<ib[j+1];kp++){
+	k = jb[kp];
+        ix[k]+=aij*b[kp];
+      }
+    }
+    for(j=ic[i];j<ic[i+1];j++) c[j] = ix[jc[j]];
+  }
+  ic[np]=nnz;
+  free(ix);
+  C->val=c; 
+  C->nnz=nnz; 
+  return;
+}
+/**************************************************************************/
+/*!
+   * \fn void icsr_mxm_symb_max (iCSRmat *A, iCSRmat *B, iCSRmat *C, INT multmax)
+   *
+   * \brief symbolic sparse matrix multiplication C=A*B (index starts with 0!!)
+   *
+   * \param A         Pointer to the iCSRmat matrix A
+   * \param B         Pointer to the iCSRmat matrix B
+   * \param C         Pointer to iCSRmat matrix equal to A*B
+   * \param multimax  value allowed in the iCSRmat matrix C, any entry that is not equal to multimax will be deleted
+   *
+   */
+void icsr_mxm_symb_max(iCSRmat *A,iCSRmat *B,iCSRmat *C,	\
+                       INT multmax)
+{
+  /* call the symbolic muultiplication. this is same as A*B, but we do
+     not call A*B bc there is no multiplication here aij=bij=1 */
+  icsr_mxm_symb(A,B,C);
+  /**/
+  INT np=C->row,nr=C->col,nnz=C->nnz; // INT nq=A->col;  
+  INT i,j,k,jp,kp;
+  INT *ia=A->IA, *ja=A->JA,*ib=B->IA, *jb=B->JA;
+  INT *ic=C->IA, *jc=C->JA;
+  /**/
+  INT *c=(INT *)calloc(nnz,sizeof(INT));
+  /**/
+  INT *ix=(INT *)calloc(nr,sizeof(INT));
   for(i=0;i<np;i++){
     for(j = ic[i];j<ic[i+1];j++){
       ix[jc[j]] = 0;
@@ -2143,19 +1917,13 @@ void icsr_mxm_symb_max(iCSRmat *A,
     }
   }
   ic[np]=nnz;
-  //  fprintf(stdout,"\nXXXX: %d,%d,%d : dXXXXXXX,%d",np,nq,nr,nnz);fflush(stdout);
   free(ix);
   free(c);
   c=NULL;
-  C->nnz=nnz;
-  C->row=np; C->col=nr;
   /**/
-  jc=realloc(jc,nnz*sizeof(INT));
-  /**/
-  C->IA=ic;
-  C->JA=jc;
-  C->val=NULL;
-  //  print_full_mat_int(1,C->nnz,C->JA,"eled");
+  C->JA=realloc(jc,nnz*sizeof(INT));
+  C->nnz=nnz; // update the number of nonzeroes
+  //  C->val=NULL; // it must be null already
   return;
 }
 
