373666163de926af8b0d2cbaf6228b5a4f359f51
Ludmil T Zikatanov <ludmil.math@gmail.com>
2020-05-29 21:03:21 -0400

    eigen example
    Former-commit-id: 741e678e95557cb2dab58b15a9e4f69c91ef9e17 [formerly 080773e5348ba600c10ce45edae9e68ed89a4327]
    Former-commit-id: 96f92f117d9e78f55685ef8b29506944193d79cf

--- haz_shutils/headmk.sh
+++ haz_shutils/headmk.sh
@@ -9,7 +9,11 @@
 set +x
 
 cat $1/src/assemble/*.c $1/src/fem/*.c \
-    $1/src/mesh/*.c $1/src/nonlinear/*.c $1/src/solver/*.c  $1/src/utilities/*.c $1/src/timestepping/*.c $1/src/interfaces/*.c  $1/src/amr/*.c $1/src/graphs/*.c \
+    $1/src/mesh/*.c $1/src/nonlinear/*.c \
+    $1/src/solver/*.c  $1/src/utilities/*.c \
+    $1/src/timestepping/*.c $1/src/interfaces/*.c  \
+    $1/src/amr/*.c $1/src/graphs/*.c \
+    $1/src/eigen/*.c \
 	| awk -v name="hazmath.h" -f mkheaders.awk > $1/include/hazmath.h
 
 ##sed -f createh.sed $1/src/amr/*.f	> $1/include/fortran_headers.h
