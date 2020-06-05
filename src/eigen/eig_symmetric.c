82a643a0611de178a936c0c5497c8acee300b685
Ludmil T Zikatanov <ludmil.math@gmail.com>
2020-06-05 09:22:05 -0400

    converted matrix storage to fortran friendly storage before the call to the lapack routine; not needed for symmetric matrices, but may be needed later for other
    Former-commit-id: 1e25d1469af2892158448df5e2221c597d697af7 [formerly 1c5de5308274ce0922319bb59827fcb416f72c66]
    Former-commit-id: 897d983a3d99e8140bcfad4584258209b005e980

--- src/eigen/eig_symmetric.c
+++ src/eigen/eig_symmetric.c
@@ -13,9 +13,11 @@ INT eigsymm(dCSRmat *A,dCSRmat *B,REAL *evalues, REAL *evectors)
 {
   INT info=-22;
 #if WITH_LAPACK
-  //   evalues must be have enough space for n reals; if evectors is a
-  //   NULL: compute only eigenvalues; otherwise evectors must be n*n
-  //   reals upon entering here.
+  /*    
+	evalues must have enough space for n reals; if evectors is a 
+	NULL: compute only eigenvalues; otherwise evectors must be n*n 
+	reals upon entering here. 
+  */
   char uplo='U',jobz='N';
   INT n=A->row;
   INT itype=1,lwork=3*n+6; // itype=1 is to solve A*x =lambda*B*x
@@ -29,6 +31,13 @@ INT eigsymm(dCSRmat *A,dCSRmat *B,REAL *evalues, REAL *evectors)
   }
   bf=calloc(n*n+lwork,sizeof(REAL));
   dcsr2full(A,af);
+  /* 
+     if the matrix is not symmetric we need to do here c2r before we
+     call the fortran to make the matrix stored "fortran friendly". We
+     use c2r here just to be consistent
+  */
+  c2r(n,n,sizeof(REAL),(void *)af);
+  /*B must be always symmetric as it should define an inner product  so no need of c2r; */
   dcsr2full(B,bf);
   work = bf + n*n;
   dsygv_( &itype, &jobz, &uplo, &n, af, &n, bf, &n, w, work,
