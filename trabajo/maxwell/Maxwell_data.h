c339d856514219f6e6187c3e43b5c1352d37823c
ccavan02 <casey.cavanaugh@tufts.edu>
2020-06-05 20:54:23 -0400

    what I have for precond so far
    Former-commit-id: 60acd13e95294d4f82b9f7ba2467d1f779bfc5a4 [formerly 26e7e792007c88006b11b1b1bc20f1115b0db2bb]
    Former-commit-id: e8f434fa75164574bef41577c11c8b5376529448

--- trabajo/maxwell/Maxwell_data.h
+++ trabajo/maxwell/Maxwell_data.h
@@ -106,3 +106,186 @@ void bc_test(REAL *val,REAL* x,REAL time,void *param) {
 
 
 
+
+/***********************************************************************************************/
+/*!
+   * \fn dCSRmat dcsr_invert_diagonal_matrix(dvector *diag)
+   *
+   * \brief create inverse of diagonal matrix using diag as diagonal entres
+   *
+   * \param diag  Pointer to the diagonal as a dvector
+   *
+   * \return D    Pointer to dCSRmat CSR inverse diagonal matrix
+   *
+   */
+dCSRmat dcsr_invert_diagonal_matrix(dvector *diag)
+{
+  //local variable
+  INT n = diag->row;
+  INT i;
+
+  // form the diaongal matrix
+  dCSRmat D = dcsr_create(n,n,n);
+  for (i=0;i<n;i++)
+  {
+    D.IA[i] = i;
+    D.JA[i] = i;
+    D.val[i]   = 1/diag->val[i];
+  }
+  D.IA[n] = n;
+
+  // return
+  return D;
+}
+
+build_precond_maxwell( dCSRmat *G, dCSRmat *Gt, dCSRmat *K , dCSRmat *Kt, 
+					dCSRmat *Mf, dCSRmat *Me, dCSRmat *Mv, dCSRmat *A_diag, block_dCSRmat *Lb)
+
+
+
+
+//-------------------------------
+  // prepare block preconditioner
+ 
+  //-------------------------------------
+  // lower block triangular for LU solve
+  //-------------------------------------
+  //block_dCSRmat Lb;
+  //Lb.brow = 3; Lb.bcol = 3;
+  //Lb.blocks = (dCSRmat **) calloc(9,sizeof(dCSRmat *));
+  
+  
+  dCSRmat IB =  dcsr_create_identity_matrix(mesh.nface,0);
+  dCSRmat IE =  dcsr_create_identity_matrix(mesh.nedge,0);
+  dCSRmat Ip =  dcsr_create_identity_matrix(mesh.nv,0);
+
+
+/*   dCSRmat IB = dcsr_create(mesh.nface, mesh.nface, mesh.nface);
+  dCSRmat IE = dcsr_create(mesh.nedge, mesh.nedge, mesh.nedge);
+  dCSRmat Ip = dcsr_create(mesh.nv, mesh.nv, mesh.nv);
+
+  for (ii=0; ii<mesh.nface; ii++) {
+    IB.IA[ii] = ii;
+    IB.JA[ii] = ii;
+    IB.val[ii] = 1.0;
+  }
+  IB.IA[mesh.nface] = mesh.nface;
+
+  for (ii=0; ii<mesh.nedge; ii++) {
+    IE.IA[ii] = ii;
+    IE.JA[ii] = ii;
+    IE.val[ii] = 1.0;
+  }
+  IE.IA[mesh.nedge] = mesh.nedge;
+
+  for (ii=0; ii<mesh.nv; ii++) {
+    Ip.IA[ii] = ii;
+    Ip.JA[ii] = ii;
+    Ip.val[ii] = 1.0;
+  }
+  Ip.IA[mesh.nv] = mesh.nv; */
+
+  Lb.blocks[0] = &IE;
+  Lb.blocks[1] = &Kt;
+  Lb.blocks[2] = NULL;
+  Lb.blocks[3] = NULL;
+  Lb.blocks[4] = &IB;
+  Lb.blocks[5] = NULL;
+  Lb.blocks[6] = &Gt;
+  Lb.blocks[7] = NULL;
+  Lb.blocks[8] = &Ip;
+
+  // Convert back to CSR and shift back
+  //dCSRmat L = bdcsr_2_dcsr(&Lb);
+  //dcsr_shift(&L,1);
+
+  // eliminate the boundary of matrix
+  eliminate_DirichletBC_blockFE(NULL,&FE,&mesh,NULL,&L,0.0);
+  //dcsr_shift(&L,-1);
+
+  // get G and K without boundary
+  dCSRmat Gtb;
+  dCSRmat Ktb;
+
+  dcsr_getblk(&L, E_idx.val, B_idx.val, mesh.nedge, mesh.nface, &Ktb);
+  dcsr_getblk(&L, p_idx.val, E_idx.val, mesh.nv,    mesh.nedge, &Gtb);
+
+  dCSRmat Kb;
+  dcsr_trans(&Ktb,&Kb);
+  dCSRmat Gb;
+  dcsr_trans(&Gtb, &Gb);
+
+  // scale offdiagonal blocks
+  dcsr_axm(&Kb,  dt/2.0);
+  dcsr_axm(&Gb,  dt/2.0);
+  dcsr_axm(&Ktb, dt/2.0);
+  dcsr_axm(&Gtb, dt/2.0);
+
+  // clean up
+  bdcsr_free(&Lb);
+  dcsr_free(&L);
+  dcsr_free(&Gt);
+  dcsr_free(&Kt);
+  dcsr_free(&IB);
+  dcsr_free(&IE);
+  dcsr_free(&Ip);
+  //-------------------------------------
+
+  // prepare diagonal blocks
+  //dCSRmat *A_diag;
+  //A_diag = (dCSRmat *)calloc(3, sizeof(dCSRmat));
+
+  // first diagonal block: 2/dt * M_f
+  dcsr_alloc(Mf.row, Mf.col, Mf.nnz, &A_diag[0]);
+  dcsr_cp(&Mf, &A_diag[0]);
+  dcsr_axm(&A_diag[0], 2.0/dt);
+
+  //dcsr_shift(&A_diag[0], 1);
+
+  eliminate_DirichletBC(NULL,&FE_B,&mesh,NULL,&A_diag[0],0.0);
+
+  //dcsr_shift(&A_diag[0], -1);
+
+  // second diagonal block: dt/2 * K^tMfK + 2/dt*Me + Z
+  dCSRmat KT;
+  dCSRmat KTMfK;
+  //dcsr_shift(&K,-1);
+  dcsr_trans(&K, &KT);
+  dcsr_rap(&KT, &Mf, &K, &KTMfK);
+  dcsr_add(&KTMfK, dt/2.0, &Me, 2.0/dt, &A_diag[1]);
+  dcsr_free(&KTMfK);
+  dcsr_alloc(A_diag[1].row, A_diag[1].col, A_diag[1].nnz, &KTMfK);
+  dcsr_cp(&A_diag[1],&KTMfK);
+  dcsr_free(&A_diag[1]);
+  //dcsr_add(&KTMfK, 1.0, &Z, 1.0, &A_diag[1]);
+  dcsr_free(&KTMfK);
+  dcsr_free(&KT);
+  //dcsr_shift(&K,1);
+
+  //dcsr_shift(&A_diag[1], 1);
+
+  eliminate_DirichletBC(NULL,&FE_E,&mesh,NULL,&A_diag[1],0.0);
+
+  //dcsr_shift(&A_diag[1], -1);
+
+  //dcsr_shift(&P_curl, -1);  // shift
+  //dcsr_shift(&Grad, -1);  // shift
+
+  // third diagonal block: dt/2 G^tMeG + 2/dt Mv
+  dCSRmat GT;
+  dCSRmat GTMeG;
+  //dcsr_shift(&G,-1);
+  dcsr_trans(&G, &GT);
+  dcsr_rap(&GT, &Me, &G, &GTMeG);
+  dcsr_add(&GTMeG, dt/2.0, &Mv, 2.0/dt, &A_diag[2]);
+  dcsr_free(&GTMeG);
+  dcsr_free(&GT);
+  //dcsr_shift(&G,1);
+
+  //dcsr_shift(&A_diag[2], 1);
+
+  eliminate_DirichletBC(NULL,&FE_p,&mesh,NULL,&A_diag[2],0.0);
+
+  //dcsr_shift(&A_diag[2], -1);
+
+
