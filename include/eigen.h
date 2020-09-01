c7a502b26507f1862a064b5f8439f5fa6e7c98af
jadler05 <james.adler@tufts.edu>
2020-09-01 13:58:30 -0400

    added routine to compute svd of a full matrix from Lapack

--- include/eigen.h
+++ include/eigen.h
@@ -1,7 +1,7 @@
 //
 //
 //  eigen.h
-//  
+//
 //
 //  Created by James Adler, Xiaozhe Hu, and Ludmil Zikatanov on 20200529
 //
@@ -12,4 +12,8 @@
 void dsygv_( INT *itype, char *jobz, char *uplo, INT *n,	\
  	     REAL *a, INT *lda, REAL *b, INT *ldb, REAL *w,	\
 	     REAL *work, INT *lwork, INT *info );
+
+void dgesvd_( char *joba, char *jobu, INT *m,	INT *n, \
+       REAL *a, INT *lda, REAL *s, REAL *u, INT *ldu, REAL *vt,	\
+       INT *ldvt, REAL *work, INT *lwork, INT *info );
 // this should be replaced by lapack.h if any exists in a standard install of lapack. 
