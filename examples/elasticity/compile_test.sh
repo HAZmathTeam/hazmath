a3eaa924e955b76f82ee0b2480bfec23c311ae7f
Ludmil T Zikatanov <ludmil.math@gmail.com>
2020-10-01 22:39:41 -0400

    merge

--- examples/elasticity/compile_test.sh
+++
@@ -1,5 +0,0 @@
-set -x
-gcc -o test_faces faces.c -I../../include -L../../lib -lm -lhazmath -lsuitesparseconfig -lcholmod -lamd -lcolamd -lccolamd -lcamd -lspqr -lumfpack -lamd -lcxsparse
-set +x
-
-
