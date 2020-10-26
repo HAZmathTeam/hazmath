a44fbea9697ff22bce36f07235b13ce44641be7b
Ludmil T Zikatanov <ludmil.math@gmail.com>
2020-10-26 00:20:22 -0400

    updated eigenvalues LAPACKs with SVD

--- include/eigen.h
+++ include/eigen.h
@@ -29,5 +29,18 @@ void dgeqrf_(INT *m, INT *n, REAL *a, INT *lda, REAL *tau, REAL* work, \
 // extract Q from QR factorization
 void dorgqr_(INT *m, INT *n1, INT *n2, REAL *a, INT *lda, REAL *tau, REAL* work, \
       INT* lwork, INT* info);
+//
+// nonsymmetric eigenvalue computations and SVD
+//
+void  dgeevx_( char *balanc, char *jobvl, char *jobvr, char *sense,	\
+	       INT *n, REAL *a, INT *lda, REAL *wr, REAL *wi,		\
+	       REAL *vl, INT *ldvl, REAL *vr, INT *ldvr,		\
+	       INT *ilo, INT *ihi, REAL *scale, REAL *abnrm,		\
+	       REAL *rconde, REAL *rcondv, REAL *work, INT *lwork,	\
+	       INT *iwork, INT *info );
 
+void  dgesvd_(char *jobu, char *jobvt, INT *m, INT *n,	\
+	      REAL *a, INT *lda, REAL *s,		\
+	      REAL *u,INT *ldu, REAL *vt, INT *ldvt,	\
+	      REAL *work,INT *lwork, INT *info);   
 // this should be replaced by lapack.h if any exists in a standard install of lapack.
