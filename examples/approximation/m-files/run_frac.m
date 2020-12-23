4a4c23830d35d018d648e7f140cfb6a3e9b0ae32
Ludmil T Zikatanov <ludmil.math@gmail.com>
2020-12-23 18:44:36 -0500

    added m-files

--- examples/approximation/m-files/run_frac.m
+++ examples/approximation/m-files/run_frac.m
@@ -14,13 +14,18 @@ x=[linspace(0,1,nh+2)]';
 x=x(2:nh+1);
 dim=1;
 [A,M,f]=matrix_setup_mass(nh,do_coeff);
-sm=dim*(dim+1)/min(diag(M));
-sa=1/norm(A,inf);
-[U,d]=eig(sa*A,sm*M);
-check_nrm=norm(sa*A*U-sm*M*U*d)
-d=diag(d);
-Ds=spdiags(d.^s,[0],nh,nh);
-As=M*U*Ds*U'*M;    
+
+sm=dim*(dim+1)/min(diag(M)); sa=1/norm(A,inf);
+
+if(nnz(A)/size(A,1)<3) 
+    [U,d]=eig(sa*A,sm*M);
+    check_nrm=norm(sa*A*U-sm*M*U*d)
+    d=diag(d);
+    Ds=spdiags(d.^s,[0],nh,nh);
+    As=M*U*Ds*U'*M;
+else
+    %% load matrix
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 bnd0=0;
 bnd1=sm*sa;
@@ -36,6 +41,7 @@ n=size(A,1);
 I=speye(n);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 rhs=randn(n,1);
+%%
 ff=M*rhs;
 asf=res(m)*ff;
 for j=1:m1
