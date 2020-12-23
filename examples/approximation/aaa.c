0a97d94ad27286a238088a61da4bcbd6db8b6a7d
Ludmil T Zikatanov <ludmil.math@gmail.com>
2020-12-23 08:20:31 -0500

    changed output of aaa

--- examples/approximation/aaa.c
+++ examples/approximation/aaa.c
@@ -32,7 +32,7 @@ INT main()
   REAL16 tolaaa=powl(2e0,-52e0);
   // parameters for the function we are approximating.
   // For example: s[2]*(x^s[0])+s[3]*(x**s[1])
-  REAL16 s[4]={0.33e0,-0.5e0,2e0,3e0}; // s1, s2, alpha,beta
+  REAL16 s[4]={-0.5e0,0.5e0,1e0,0e0}; // s1, s2, alpha,beta
   //  s[0]=0.5;  s[1]=-0.5;  s[2]=1e0;  s[3]=2e0;
   //
   REAL **cpzwf=malloc(5*sizeof(REAL *));
@@ -57,7 +57,14 @@ INT main()
 		      &m,xmin_in,xmax_in,tolaaa,print_level);
   //rmax is the max error on the rest of z
   INT mem,mem16,i,j,k,m1=m+1,m01=m-1,mm=m*m,mm1=m1*m1;  
-  fprintf(stdout,"\n\nm=%d; max_err=%.12e\n",m,rmax);
+  fprintf(stdout,"\n%%%%EXAMPLE(fractional):\n");
+  fprintf(stdout,"\nfrac = @(x) ( %.2Lf * x.^(%.1Lf) + %.2Lf * x.^(%.1Lf) );\n",s[2],s[0],s[3],s[1]);
+  fprintf(stdout,"\n%%===============================================%%\n");
+  fprintf(stdout,"\nbounds=[%.16e,%.16e];\n",xmin_in,xmax_in);
+  fprintf(stdout,"\n\nm=%d; max_err=%.12e;\n",m,rmax);
+  fprintf(stdout,"\npol=zeros(%d,1);res=zeros(%d,1);\nz=zeros(%d,1);w=zeros(%d,1);f=zeros(%d,1);\n", \
+	  m-1,m,m,m,m);
+  fprintf(stdout,"\n%%===============================================%%\n");
   
   for(i=0;i<m;i++)fprintf(stdout,"\nres(%d)=%.16e;",i+1,*(cpzwf[0]+i));
   fprintf(stdout,"\n");
@@ -69,6 +76,9 @@ INT main()
   fprintf(stdout,"\n");
   for(i=0;i<m;i++)fprintf(stdout,"\nf(%d)=%.16e;",i+1,*(cpzwf[4]+i));
   fprintf(stdout,"\n");
+  fprintf(stdout,"\ner=norm(f-frac(z))");
+  fprintf(stdout,"\n");
+  fprintf(stdout,"\n%%===============================================%%\n\n");
   free(cpzwf[0]);// that is enough. the rest 1-4 are just shifts of cpzwf[0] with m+1;
   free(cpzwf);
   return 0;  
