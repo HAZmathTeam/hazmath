adf2562dbe0d4526f310809e4f5ce4f81bb977f1
Ludmil T Zikatanov <ludmil.math@gmail.com>
2020-12-24 11:33:49 -0500

    changed examples/appproximation and src/approximation aaa algorithm to have some tests

--- examples/approximation/aaa.c
+++ examples/approximation/aaa.c
@@ -17,7 +17,7 @@
 /*|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
 #include "aaa_example.h"
 /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
-INT main()
+INT main(int argc,char *argv[])
 {
   REAL xmin_in=0e0,xmax_in=1e0;
   ///////////////////////////////////////////////////////////////////////////
@@ -27,14 +27,22 @@ INT main()
   //
   // m is the number of nodes in the final interpolation after
   // tolerance tolaaa is achieved or mmax is reached.
-  INT mbig=(1<<10)+1;
+  INT mbig=(1<<14)+1;
   INT mmax_in=(INT )(mbig/2),m=-22;
   REAL16 tolaaa=powl(2e0,-52e0);
   // parameters for the function we are approximating.
   // For example: s[2]*(x^s[0])+s[3]*(x**s[1])
   REAL16 s[4]={-0.5e0,0.5e0,1e0,0e0}; // s1, s2, alpha,beta
-  //  s[0]=0.5;  s[1]=-0.5;  s[2]=1e0;  s[3]=2e0;
   //
+  INT mem,mem16,i,j,k,m1=m+1,m01=m-1,mm=m*m,mm1=m1*m1;  
+  // s[0]=s,s[1]=t,s[2]=alpha,s[3]=beta;
+  fprintf(stderr,"\nUSAGE: ./aaa.ex<<EOF_FRAC\n s,t,alpha,beta,xmin,xmax\nEOF_FRAC\n");
+  fprintf(stderr,"\nEXAMPLE:\n./aaa.ex <<EOF_FRAC >some_file.m\n %.2Lf %.2Lf %.2Lf %.2Lf %.2f %.2f\nEOF_FRAC\n", \
+	  s[0],s[1],s[2],s[3],xmin_in,xmax_in);
+  k=fscanf(stdin,"%Lg %Lg %Lg %Lg %lg %lg",&s[0],&s[1],&s[2],&s[3],&xmin_in,&xmax_in);  
+  /////////////////////////////////////////////  
+  //  s[0]=0.5;  s[1]=-0.5;  s[2]=1e0;  s[3]=2e0;
+  /////////////////////////////////////////////////////////////////////////////////
   REAL **cpzwf=malloc(5*sizeof(REAL *));
   // after a call to get_cpzwf, cpzwf[k] are pointers to the following
   // arrays (each of m+1 elements but at most m are used:
@@ -56,9 +64,12 @@ INT main()
 		      &mbig,&mmax_in,				\
 		      &m,xmin_in,xmax_in,tolaaa,print_level);
   //rmax is the max error on the rest of z
-  INT mem,mem16,i,j,k,m1=m+1,m01=m-1,mm=m*m,mm1=m1*m1;  
+  fprintf(stdout,"\nfunction [res,pol,z,w,f,er]=frac();\n");
+  fprintf(stdout,"\n%% AUTO GENERATED\n");
   fprintf(stdout,"\n%%%%EXAMPLE(fractional):\n");
-  fprintf(stdout,"\nfrac = @(x) ( %.2Lf * x.^(%.1Lf) + %.2Lf * x.^(%.1Lf) );\n",s[2],s[0],s[3],s[1]);
+  fprintf(stdout,"\n%%%%[res,pol,z,w,f,er]=frac();\n");
+  //  fprintf(stdout,"\n%%f_in = @(x) ( %.2Lf * x.^(%.1Lf) + %.2Lf * x.^(%.1Lf) );\n",s[2],s[0],s[3],s[1]);
+  fprintf(stdout,"\n%%f_in = %.2Lf * x.^(%.1Lf) + %.2Lf * x.^(%.1Lf) ;\n",s[2],s[0],s[3],s[1]);
   fprintf(stdout,"\n%%===============================================%%\n");
   fprintf(stdout,"\nbounds=[%.16e,%.16e];\n",xmin_in,xmax_in);
   fprintf(stdout,"\n\nm=%d; max_err=%.12e;\n",m,rmax);
@@ -76,8 +87,9 @@ INT main()
   fprintf(stdout,"\n");
   for(i=0;i<m;i++)fprintf(stdout,"\nf(%d)=%.16e;",i+1,*(cpzwf[4]+i));
   fprintf(stdout,"\n");
-  fprintf(stdout,"\ner=norm(f-frac(z))");
-  fprintf(stdout,"\n");
+  fprintf(stdout,"\nfz = %.2Lf * z.^(%.1Lf) + %.2Lf * z.^(%.1Lf);\n",s[2],s[0],s[3],s[1]);
+  fprintf(stdout,"\ner=norm(f-fz);");
+  fprintf(stdout,"\nreturn;\nend;\n");
   fprintf(stdout,"\n%%===============================================%%\n\n");
   free(cpzwf[0]);// that is enough. the rest 1-4 are just shifts of cpzwf[0] with m+1;
   free(cpzwf);
--- examples/approximation/m-files/run_frac.m
+++ examples/approximation/m-files/run_frac.m
@@ -1,21 +1,17 @@
-do_coeff=0;
-levmin=3;levmax=3;
-ea=zeros(levmax-levmin+1,1);
-em=ea;
-s=-0.5;
-for lev=levmin:levmax
-    nh=2^lev-1;
-    x=[linspace(0,1,nh+2)]';
-    x=x(2:nh+1);
-    [A,M,f]=matrix_setup_mass(nh,do_coeff);
-    sm=1/norm(M,inf);
-    sa=1/norm(A,inf);
-    A=sa*A;
-    M=sm*M;
-    [U,d]=eig(A,M);
-    norm(A*U-M*U*d)
-    pause
-    d=diag(d);
-    Ds=spdiags(d.^s,[0],nh,nh)
-    As=M*U*Ds*U'*M;    
-end
+%% APPROXIMATES inv(M*U*D^s*U*M), where A*U=M*U*D.
+%% KEEP alpha beta and t as they are below or change 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+alpha = 1.0; beta  = 0.0; t   =  0.5; s=-0.5;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% load matrices and vectors all symmetric:
+A=get_matrix_haz('A',1);
+M=get_matrix_haz('M',1);
+%%%%%%%%%%%%%%%%%%%%%%%%%Af=get_matrix_haz('Af',1);
+b=get_vector_haz('b');
+u=get_vector_haz('u');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% COMPUTE APPROXIMATION:
+dim=2;
+ur=approx_frac(A,M,b,s,dim);
+fprintf(1,'==========================\n |u-ur|=%.12e\n==========================\n',norm(ur-u,inf))
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
