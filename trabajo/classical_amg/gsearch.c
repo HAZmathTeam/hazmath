fde661017936ee3abbf6a62ae483b68760aed6a1
Ludmil T Zikatanov <ludmil.math@gmail.com>
2021-04-16 01:01:04 -0400

    added bfs for testing

--- trabajo/classical_amg/gsearch.c
+++ trabajo/classical_amg/gsearch.c
@@ -7,7 +7,7 @@
  *
  */
 #include "hazmath.h"
-/*********************************************************************/
+/*************************************************************************/
 // main
 void main()
 {
@@ -17,15 +17,45 @@ void main()
   fprintf(stdout,"\nDepth First Search permutation:\n");
   INT i,k;
   // find the connected components
-  iCSRmat *blk=haz_dfs(a);
-  fprintf(stdout,"\nend of list: blk->IA[%d]=%d (.eq. %d \?)\n",blk->row,blk->IA[blk->row],blk->row);
-  for(i=0;i<blk->row;i++){
-    fprintf(stdout,"\nblock[%d]; size=%d:  ",i,blk->IA[i+1]-blk->IA[i]);
-    for(k=blk->IA[i];k<blk->IA[i+1];k++){ 
-      fprintf(stdout,"%d ",blk->JA[k]);
-    } 
-  }  
-  fprintf(stdout,"\nblocks=%d; vertices=%d; vertices(orig)=%d\n",blk->row,blk->col,a->col);
+  iCSRmat *blk_dfs=haz_dfs(a);
+  fprintf(stdout,"\nend of list: blk_dfs->IA[%d]=%d (.eq. %d \?)\n",blk_dfs->row,blk_dfs->IA[blk_dfs->row],blk_dfs->row);
+  /* for(i=0;i<blk_dfs->row;i++){ */
+  /*   fprintf(stdout,"\nblock[%d]; size=%d:  ",i,blk_dfs->IA[i+1]-blk_dfs->IA[i]); */
+  /*   for(k=blk_dfs->IA[i];k<blk_dfs->IA[i+1];k++){  */
+  /*     fprintf(stdout,"%d ",blk_dfs->JA[k]); */
+  /*   }  */
+  /* }   */
+  fprintf(stdout,"\nblocks=%d; vertices=%d; vertices(orig)=%d\n",blk_dfs->row,blk_dfs->col,a->col);
+
+  /*******************************************************************/
+  ivector *roots=malloc(1*sizeof(ivector));
+  // grab a point in every connected component:
+  roots->row=blk_dfs->row;
+  roots->val=(INT *)calloc(roots->row,sizeof(INT));
+  for(i=0;i<blk_dfs->row;i++){
+    k=blk_dfs->IA[i];
+    roots->val[i]=blk_dfs->JA[k];
+  }
+  ivector *anc=malloc(1*sizeof(ivector));
+  INT level_max=a->row+1;
+  fprintf(stdout,"BFS:");
+  iCSRmat *blk_bfs=run_bfs(a,roots,anc,level_max);
+  fprintf(stdout,"\nAnother ord::");
+  for(i=0;i<blk_bfs->row;i++){
+    fprintf(stdout,"\nrow=%d: ",i);
+    for(k=blk_bfs->IA[i];k<blk_bfs->IA[i+1];++k){
+      fprintf(stdout,"%7d(%5d)",blk_bfs->JA[k],anc->val[blk_bfs->JA[k]]);
+      //blk_bfs->val[blk_bfs->JA[k]]);
+    }
+  }
+  fprintf(stdout,"\nBFS: levels=%d;vertices=%d; vertices(orig)=%d\n",blk_bfs->row,blk_bfs->IA[blk_bfs->row],a->col);
+  fprintf(stdout,"\n");
+  ivec_free(anc);
+  ivec_free(roots);
+  icsr_free(blk_dfs);
+  icsr_free(blk_bfs);
+  dcsr_free(a);
+  /*******************************************************************/
   return;
 }
 /*END********************************************************************/
