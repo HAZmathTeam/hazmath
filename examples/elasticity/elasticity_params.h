811858da10ccc7fea3688afb3fcd5c28107edfa0
Sanghyun Lee <lee@lee-mbp13.local>
2021-05-29 10:27:48 +0900

    SLee May 29th. Changes for Elasticity NewTerm

--- examples/elasticity/elasticity_params.h
+++ examples/elasticity/elasticity_params.h
@@ -3,7 +3,7 @@
 #endif
 
 #ifndef LAME_LAMBDA_GLOBAL
-#define LAME_LAMBDA_GLOBAL 1
+#define LAME_LAMBDA_GLOBAL 1000000
 #endif
 
 #ifndef PENALTY_PARAMETER_GLOBAL
@@ -11,7 +11,7 @@
 #endif
 
 #ifndef TOTAL_NUM_CYCLES_GLOBAL
-#define TOTAL_NUM_CYCLES_GLOBAL 6
+#define TOTAL_NUM_CYCLES_GLOBAL 5
 #endif
 
 // the LAME_MU_GLOBAL is not used in the code yet...
