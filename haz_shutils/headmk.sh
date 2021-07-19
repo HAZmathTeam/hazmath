a5c06557b0c77d334c7a1a7257f4f0b25126c8ab
Ludmil T Zikatanov <ludmil.math@gmail.com>
2021-07-18 21:48:53 -0400

    added to CMakeLists.txt few lines to create  .h file with the functions in stc/haznics/helper.c

--- haz_shutils/headmk.sh
+++ haz_shutils/headmk.sh
@@ -3,9 +3,7 @@
 # usage is headmk.sh ${CMAKE_CURRENT_SOURCE_DIR} where we assume that
 # ${CMAKE_CURRENT_SOURCE_DIR} has subdirectories src and include and
 # the function names from src/*/*.c are put into the include/hazmath.h
-# commented as is not needed for now (ltz, 20190730) FORTRAN: the
-# subroutine names src/amr/*.f are put into the
-# include/fortran_headers.h
+# commented as is not needed for now (ltz, 20190730)
 #set +x
 
 cat $1/src/assemble/*.c $1/src/fem/*.c \
