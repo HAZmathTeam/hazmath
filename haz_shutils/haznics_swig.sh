a98b1b2bf673d2c22f42b210dd068f1f98573ef4
anabudisa <anbudisa@gmail.com>
2021-07-22 11:42:05 +0200

    update install script for hazmath-python

--- haz_shutils/haznics_swig.sh
+++ haz_shutils/haznics_swig.sh
@@ -3,24 +3,28 @@
 # Ana Budisa - 2021-07-15
 
 # Update cmake if needed
-echo "Installing cmake v >= 3.20..."
-sudo snap install cmake
+echo "Installing cmake v >= 3.16..."
+sudo apt-get update
+sudo apt-get install cmake
 echo "... Done."
 
 # Install swig4.0
-echo "Installing swig 4.0.2..."
-wget http://prdownloads.sourceforge.net/swig/swig-4.0.2.tar.gz
-tar xvzf swig-4.0.2.tar.gz
-cd swig-4.0.2
-wget https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.gz
-./configure
-make
-sudo make install
-cd ..
+echo "Installing swig v >= 4.0..."
+sudo apt-get install swig
 echo "... Done."
 
 # Install suitesparse if needed
 echo "Installing suitesparse..."
-sudo apt-get update
 sudo apt install -y libsuitesparse-dev
+echo "... Done."
+
+# Install python-dev
+echo "Installing python3-dev..."
+sudo apt-get install -y python3-dev
+echo "... Done."
+
+# Install numpy
+echo "Installing numpy on python3..."
+sudo python3 -m pip install --upgrade pip
+sudo pip install numpy
 echo "... Done."
