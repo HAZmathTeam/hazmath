fa42723ecaff6b5f58f8be07d215bd8dac1503fa
Ludmil T Zikatanov <ludmil.math@gmail.com>
2021-07-25 11:52:29 -0400

    removed more trabajo

--- trabajo/signal_graphs/tests/get_random_prolongation_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "../hamiltonian_algorithm.h"
-#include "utils.h"
-
-int main() {
-  auto samples = getRandomProlongation(3, 5, 0);
-  assertArraysEqual(samples.data(), std::vector<int>({0, 0, 1, 2, 2}));
-
-  return 0;
-}
