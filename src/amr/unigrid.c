0d80e757d4f1495ef4892b2628b86b4000c0ee3a
jadler05 <james.adler@tufts.edu>
2022-10-11 23:41:26 -0400

    fix warning in unigrid.c

--- src/amr/unigrid.c
+++ src/amr/unigrid.c
@@ -4,12 +4,12 @@
  *  Copyright 2017__HAZMATH__. All rights reserved.
  *
  *   \note routines to initialize construct and destruct uniform grids
- *  in d-dimensions for general d. 
+ *  in d-dimensions for general d.
 */
 #include "hazmath.h"
 /**********************************************************************/
 /*!
- * \fn void coord_lattice(INT *m,const INT dim,const INT kf, 
+ * \fn void coord_lattice(INT *m,const INT dim,const INT kf,
  *                        const INT nall, const INT *nd)
  *
  * \brief given a global number kf on a lattice grid with
@@ -18,7 +18,7 @@
  *        vertices in the lattice, nd[i] is the number of divisions in
  *        every direction;
  *
- * \param 
+ * \param
  *
  * \return
  *
@@ -46,7 +46,7 @@ void coord_lattice(INT *m,const INT dim,const INT kf, \
  *        lexicographically ordered lattice grid. Divisions in each
  *        directions are stored in nd[].
  *
- * \param 
+ * \param
  *
  * \return
  *
@@ -68,7 +68,7 @@ INT num_lattice(INT *m,const INT dim,INT *nd)
 
 void binary1(const INT dim, unsigned INT *bits, INT *nvloc)
 {
-  // coordinates of the vertices of the unit dim-cube as arrays of 0/1 
+  // coordinates of the vertices of the unit dim-cube as arrays of 0/1
   INT i,k,kdim=-10,nbits=dim-1;
   *nvloc = (1 << dim);
   for(k = 0;k<(*nvloc);k++){
@@ -82,7 +82,7 @@ void binary1(const INT dim, unsigned INT *bits, INT *nvloc)
 
 /**********************************************************************/
 /*!
- * \fn scomplex *umesh(const INT dim,INT *nd, cube2simp *c2s,\ 
+ * \fn scomplex *umesh(const INT dim,INT *nd, cube2simp *c2s,\
  *                    INT *labelf,INT *isbndf, INT *codef,INT elflag, \
  *                    const INT intype)
  *
@@ -93,7 +93,7 @@ void binary1(const INT dim, unsigned INT *bits, INT *nvloc)
  *        isomorphic to the cube via a bilinear or "Q2" change of
  *        coordinates. output is a simplicial complex sc.
  *
- * \param intype: type of uniform grid. 
+ * \param intype: type of uniform grid.
  *
  *    if(intype == -2) use unirefine() function (from unigrid.c in
  *   src/amr) if(intype == -1)construct grid using diagonals pointing
@@ -101,7 +101,7 @@ void binary1(const INT dim, unsigned INT *bits, INT *nvloc)
  *   mesh is constructed like criss-cross grid. This works in 2D and
  *   3D, and is unclear whether it works in d>3.
  *
- * \return scomplex containing the simplicial grid. 
+ * \return scomplex containing the simplicial grid.
  *
  * \note
  *
@@ -118,7 +118,7 @@ scomplex *umesh(const INT dim,					\
   // m is dim+1 so that we can handle even dimensions
   INT *m = (INT *)calloc(dim1,sizeof(INT));
   INT *mm = (INT *)calloc(dim1,sizeof(INT));
-  INT *cnodes = (INT *)calloc(c2s->nvcube,sizeof(INT));  
+  INT *cnodes = (INT *)calloc(c2s->nvcube,sizeof(INT));
   //  INT *icycle = (INT *)calloc(dim+1,sizeof(INT));
   INT nv=1,ns=1;
   for(i=0;i<dim;i++){
@@ -127,7 +127,7 @@ scomplex *umesh(const INT dim,					\
   }
   ns*=c2s->ns; /*multiply by the number of simplices in the unit cube
 		 (2 in 2D and 6 in 3d and 24 in 4d*/
-  scomplex *sc = (scomplex *)haz_scomplex_init(dim,ns,nv,dim); 
+  scomplex *sc = (scomplex *)haz_scomplex_init(dim,ns,nv,dim);
   for(kf=0;kf<sc->nv;kf++){
     coord_lattice(m,dim,kf,sc->nv,nd);
     for(i=0;i<dim;i++){
@@ -139,7 +139,7 @@ scomplex *umesh(const INT dim,					\
 	 first. That is why we also have (dim-i-1) instead of i*/
       sc->x[kf*dim+(dim-i-1)]=((REAL )m[i])/((REAL )nd[i]);
       /* OLD: sc->x[kf*dim+i]=((REAL )m[i])/((REAL )nd[i]); */
-    }    
+    }
   }
   ns=0;
   for(kf=0;kf<sc->nv;kf++){
@@ -159,7 +159,7 @@ scomplex *umesh(const INT dim,					\
       for(i=1;i<dim-1;i++){
 	type+=2*(m[i]%2);
       }
-      // this is a hack here to work in 2D. unclear how to do in 2D yet or 4D. 
+      // this is a hack here to work in 2D. unclear how to do in 2D yet or 4D.
       if(dim==2){type=(abs(m[1]-m[0])+intype)%dim;}
       if((m[dim-1]%2)) type=dim-type;
       if((!(dim%2)) && (type>=(dim))) {type%=(dim);}
@@ -173,7 +173,7 @@ scomplex *umesh(const INT dim,					\
     	mm[i]=m[i]+(c2s->bits[dim*j+i]);
       }
       cnodes[j]=num_lattice(mm,dim,nd);
-    }   
+    }
     for(i=0;i<c2s->ns;i++){
       for(j=0;j<dim1;j++){
 	iz1=c2s->nodes[i*dim1+j];
@@ -181,14 +181,14 @@ scomplex *umesh(const INT dim,					\
 	sc->nodes[ns*dim1+j]=cnodes[jperm];
       }
       sc->flags[ns]=elflag;
-      ns++;      
-    }    
+      ns++;
+    }
   }
   INT cfbig=((INT )MARKER_BOUNDARY_NO)+100;
   INT facei,bf,cf,mi;
   //  INT kfp,ijk,mi,mip,toskip,toadd;
   /******************************************************************/
-  /*  
+  /*
    *  when we come here, all boundary faces have codes and they are
    *  non-zero. All interior faces should have a code zero.
    */
@@ -227,7 +227,7 @@ scomplex *umesh(const INT dim,					\
   sc->bndry_v->IA[0]=0;
   for(kf=0;kf<sc->nv;++kf){
     /* from 1 to n this holds the address of the beginning of the previous row */
-    sc->bndry_v->IA[kf+1]=sc->bndry_v->IA[kf]+sc->n; 
+    sc->bndry_v->IA[kf+1]=sc->bndry_v->IA[kf]+sc->n;
   }
   /**/
   for(facei=0;facei<c2s->nf;facei++){
@@ -245,7 +245,7 @@ scomplex *umesh(const INT dim,					\
 	if(m[mi]==bf){
 	  if(sc->bndry[kf]>cf) sc->bndry[kf]=cf;
 	}
-      } 
+      }
     }
     for(kf=0;kf<sc->nv;kf++){
       coord_lattice(m,dim,kf,sc->nv,nd);
@@ -254,15 +254,15 @@ scomplex *umesh(const INT dim,					\
 	sc->bndry_v->JA[j]=facei;
 	sc->bndry_v->IA[kf]++;
 	//	fprintf(stdout,"\nvertex=%d; face=%d; j=%d; bf=%d; cf=%d; bndry=%d,label=%d",kf,facei,j,bf,cf,isbndf[facei],labelf[facei]);
-      } 
+      }
     }
   }
   fflush(stdout);
   //return IA to previous state.
-  sc->bndry_v->IA[0]=0;  
+  sc->bndry_v->IA[0]=0;
   for(kf=0;kf<sc->nv;kf++)
     sc->bndry_v->IA[kf+1]=sc->bndry_v->IA[kf]+sc->n;
-  INT row_begin=sc->bndry_v->IA[0],nnz=0;  
+  INT row_begin=sc->bndry_v->IA[0],nnz=0;
   for(kf=0;kf<sc->nv;kf++){
     for(j=row_begin;j<sc->bndry_v->IA[kf+1];++j){
       if(sc->bndry_v->JA[j]<0) continue;
@@ -288,7 +288,7 @@ scomplex *umesh(const INT dim,					\
   /******************************************************************/
   // Only interior points should now be left; set them to 0:
   for(kf=0;kf<sc->nv;kf++)
-    if(sc->bndry[kf]>=cfbig) sc->bndry[kf]=0;      
+    if(sc->bndry[kf]>=cfbig) sc->bndry[kf]=0;
   /*clean up*/
   if(m) free(m);
   if(mm) free(mm);
@@ -298,7 +298,7 @@ scomplex *umesh(const INT dim,					\
 }
 /**********************************************************************/
 /*!
- * \fn void unirefine(INT *nd,scomplex *sc)  
+ * \fn void unirefine(INT *nd,scomplex *sc)
  *
  * \brief refine uniformly l levels, where 2^l>max_m nd[m] using the
  *        generic algorithm for refinement.  Works in the following
@@ -306,16 +306,16 @@ scomplex *umesh(const INT dim,					\
  *        such that 2^{l}>max_m nd[m]. then remove all x such that
  *        x[k]>nd[k]*2^{-l} and then remap to the unit square.
  *
- * \param 
+ * \param
  *
  * \return
  *
  * \note  not used; (20180718)--ltz
  *
  */
-void unirefine(INT *nd,scomplex *sc)  
+void unirefine(INT *nd,scomplex *sc)
 {
-/* 
+/*
 */
   INT ndmax=-1,i=-1,j=-1;
   for(i=0;i<sc->n;i++)
@@ -333,7 +333,6 @@ void unirefine(INT *nd,scomplex *sc)
   INT *wrk=calloc(5*(sc->n+2),sizeof(INT));
   /* construct bfs tree for the dual graph */
   abfstree(0,sc,wrk,0);
-  free(wrk);
   ref_levels=0;
   if(ref_levels<=0) return;
   INT nsold,print_level=0;//ns,nvold,level;
@@ -346,6 +345,7 @@ void unirefine(INT *nd,scomplex *sc)
     //    haz_scomplex_print(sc,0,__FUNCTION__);fflush(stdout);
     //    exit(100);
   }
+	free(wrk);
   //INT n=sc->n, n1=n+1,level=0;
   fprintf(stdout,"refine: ");
   while(sc->level < ref_levels && TRUE){
@@ -356,7 +356,7 @@ void unirefine(INT *nd,scomplex *sc)
       if(sc->marked[j] && (sc->child0[j]<0||sc->childn[j]<0))
 	haz_refine_simplex(sc, j, -1);
     /* new mesh */
-    //    ns=sc->ns; 
+    //    ns=sc->ns;
     sc->level++;
     fprintf(stdout,"u%lldu",(long long int )sc->level);//,nsold,ns,nv);
   }
@@ -370,7 +370,7 @@ void unirefine(INT *nd,scomplex *sc)
  *
  * \brief init structure unigrid
  *
- * \param 
+ * \param
  *
  * \return
  *
@@ -381,9 +381,9 @@ unigrid *ugrid_init(INT n, INT *nd, REAL *xo, REAL *xn)
 {
   INT j;
   unigrid *ug=(unigrid *)malloc(sizeof(unigrid));
-  ug->n=n; 
-  ug->nvcube=(1<<n); 
-  ug->ndiv=nd; 
+  ug->n=n;
+  ug->nvcube=(1<<n);
+  ug->ndiv=nd;
   ug->bits=(unsigned INT *)calloc(n*(ug->nvcube),sizeof(unsigned INT));
   binary1(n,ug->bits,&(ug->nvcube));
  if(!xo || !xn){
@@ -391,7 +391,7 @@ unigrid *ugrid_init(INT n, INT *nd, REAL *xo, REAL *xn)
 					     xo[dim]*/
    ug->xn=(REAL *)calloc(n,sizeof(REAL)); /* coordinates of the max
 					     corner(NE in 2D)
-					     xn[dim]*/   
+					     xn[dim]*/
    for(j=0;j<n;j++){
      ug->xo[j]=0.;
      ug->xn[j]=1.;
@@ -412,8 +412,8 @@ unigrid *ugrid_init(INT n, INT *nd, REAL *xo, REAL *xn)
    fprintf(stdout,"\n***ERROR: could not allocate memory for a uniform grid with data in %s\n",__FUNCTION__);fflush(stdout);
    exit(7);
  }
- ug->ugtype = 0; 
- return ug; 
+ ug->ugtype = 0;
+ return ug;
 }
 void ugrid_free(unigrid *ug)
 {
@@ -423,7 +423,7 @@ void ugrid_free(unigrid *ug)
   ug->data=NULL;
   if(ug) free(ug);
   ug=NULL;
-  return; 
+  return;
 }
 /**********************************************************************/
 /*!
@@ -432,7 +432,7 @@ void ugrid_free(unigrid *ug)
  * \brief transform the unform grid ug to be over a parallelepiped
  *        with corners xodst and xndst
  *
- * \param 
+ * \param
  *
  * \return
  *
@@ -448,7 +448,7 @@ void ugrid_transform(const INT n,unigrid *ug,REAL *xodst, REAL *xndst)
   ug->xo=xodst;
   ug->xn=xndst;
   for(j=0;j<n;j++){
-    /*    
+    /*
 	  dxsrc=ug->xn[j]-ug->xo[j];
 	  dxdst=xndst[j]-xodst[j];
 	  a=dxdst/dxsrc; b=(xodst[j]*ug->xn[j]-xndst[j]*ug->xo[j])/dxsrc;
